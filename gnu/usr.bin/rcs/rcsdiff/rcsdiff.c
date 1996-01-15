09cc82af8cdf854ff290e815fd4ec9fa6a4ccfcf
niklas <niklas>
1996-01-15 21:40:37 +0000

    Fix typo so two -L options can be fed to rcsdiff.

--- gnu/usr.bin/rcs/rcsdiff/rcsdiff.c
+++ gnu/usr.bin/rcs/rcsdiff/rcsdiff.c
@@ -28,6 +28,9 @@ Report problems and direct all questions to:
 
 /*
  * $Log: rcsdiff.c,v $
+ * Revision 1.1.1.1  1995/10/18 08:41:03  deraadt
+ * initial import of NetBSD tree
+ *
  * Revision 1.3  1995/02/24 02:25:35  mycroft
  * RCS 5.6.7.4
  *
@@ -147,7 +150,7 @@ static int exitstatus;
 static RILE *workptr;
 static struct stat workstat;
 
-mainProg(rcsdiffId, "rcsdiff", "$Id: rcsdiff.c,v 1.3 1995/02/24 02:25:35 mycroft Exp $")
+mainProg(rcsdiffId, "rcsdiff", "$Id: rcsdiff.c,v 1.1.1.1 1995/10/18 08:41:03 deraadt Exp $")
 {
     static char const cmdusage[] =
 	    "\nrcsdiff usage: rcsdiff -ksubst -q -rrev1 [-rrev2] -Vn -xsuff -zzone [diff options] file ...";
@@ -208,7 +211,7 @@ mainProg(rcsdiffId, "rcsdiff", "$Id: rcsdiff.c,v 1.3 1995/02/24 02:25:35 mycroft
 		    /* fall into */
 	    case 'C': case 'F': case 'I': case 'L': case 'W':
 #if DIFF_L
-		    if (c == 'L'  &&  ++file_labels == 2)
+		    if (c == 'L'  &&  file_labels++ == 2)
 			faterror("too many -L options");
 #endif
 		    *dcp++ = c;
