3f179919ef7f48c066c696028a466b4045407312
deraadt <deraadt>
1996-04-24 22:00:41 +0000

    use yp.h instead of yp_prot.h

--- usr.sbin/ypserv/yptest/yptest.c
+++ usr.sbin/ypserv/yptest/yptest.c
@@ -28,7 +28,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$Id: yptest.c,v 1.1 1995/11/01 16:56:40 deraadt Exp $";
+static char rcsid[] = "$Id: yptest.c,v 1.2 1996/01/20 00:37:58 chuck Exp $";
 #endif
 
 #include <stdio.h>
@@ -36,7 +36,7 @@ static char rcsid[] = "$Id: yptest.c,v 1.1 1995/11/01 16:56:40 deraadt Exp $";
 #include <strings.h>
 #include <rpc/rpc.h>
 #include <rpc/xdr.h>
-#include <rpcsvc/yp_prot.h>
+#include <rpcsvc/yp.h>
 #include <rpcsvc/ypclnt.h>
 
 static int
@@ -101,8 +101,8 @@ main()
   case 0:
   	for(y=ypml; y; ) {
 		ypml=y;
-		printf("%s\n",ypml->ypml_name);
-		y=ypml->ypml_next;
+		printf("%s\n",ypml->map);
+		y=ypml->next;
 	}
   }
 
