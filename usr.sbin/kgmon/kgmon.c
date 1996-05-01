368f270d8da52d799a607b92fc43c61d8da6be6b
niklas <niklas>
1996-05-01 22:15:24 +0000

    Our kernel is named /bsd, and we use $OpenBSD$ RCS IDs

--- usr.sbin/kgmon/kgmon.c
+++ usr.sbin/kgmon/kgmon.c
@@ -1,3 +1,5 @@
+/*	$OpenBSD: src/usr.sbin/kgmon/kgmon.c,v 1.2 1996/05/01 22:15:24 niklas Exp $	*/
+
 /*
  * Copyright (c) 1983, 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -39,7 +41,7 @@ static char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)kgmon.c	8.1 (Berkeley) 6/6/93";*/
-static char *rcsid = "$Id: kgmon.c,v 1.6 1994/12/23 16:45:33 cgd Exp $";
+static char *rcsid = "$OpenBSD: src/usr.sbin/kgmon/kgmon.c,v 1.2 1996/05/01 22:15:24 niklas Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
