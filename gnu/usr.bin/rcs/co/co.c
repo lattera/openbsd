402c8afec04f96c985bf8dc67e21de0a0c66cb82
mickey <mickey>
1996-05-07 09:52:38 +0000

    fix comments s/$Id$/Id/, and usage string.

--- gnu/usr.bin/rcs/co/co.c
+++ gnu/usr.bin/rcs/co/co.c
@@ -28,6 +28,10 @@ Report problems and direct all questions to:
 
 /*
  * $Log: co.c,v $
+ * Revision 1.2  1996/04/19 12:40:04  mickey
+ * -L<string> option added to support LOCALID behaviour.
+ * maybe set up in RCSINIT environment variable.
+ *
  * Revision 1.1.1.1  1995/10/18 08:41:00  deraadt
  * initial import of NetBSD tree
  *
@@ -191,10 +195,10 @@ static struct hshentries *gendeltas;	/* deltas to be generated	*/
 static struct hshentry *targetdelta;	/* final delta to be generated	*/
 static struct stat workstat;
 
-mainProg(coId, "co", "$Id: co.c,v 1.1.1.1 1995/10/18 08:41:00 deraadt Exp $")
+mainProg(coId, "co", "$Id: co.c,v 1.2 1996/04/19 12:40:04 mickey Exp $")
 {
 	static char const cmdusage[] =
-		"\nco usage: co -{fIlMpqru}[rev] -ddate -jjoins -ksubst -sstate -T -w[who] -Vn -xsuff -zzone file ...";
+		"\nco usage: co -{fIlMpqru}[rev] -ddate -jjoins -ksubst -sstate -T -w[who] -Vn -xsuff -zzone -LlocalId file ...";
 
 	char *a, **newargv;
 	char const *author, *date, *rev, *state;
@@ -310,7 +314,7 @@ mainProg(coId, "co", "$Id: co.c,v 1.1.1.1 1995/10/18 08:41:00 deraadt Exp $")
 			zone_set(a);
 			break;
 
-		case 'L':    /*  add local $Id$ keyword string */
+		case 'L':    /*  add local 'Id' keyword string */
 			setRCSlocalId(*argv+2);
 			break;
 
