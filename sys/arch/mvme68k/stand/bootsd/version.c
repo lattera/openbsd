e7802d5f72e26ca3f4264a51fd368d9a94a907f9
chuck <chuck>
1996-05-16 02:46:57 +0000

    update to run from bootxx + major clean up

--- sys/arch/mvme68k/stand/bootsd/version.c
+++ sys/arch/mvme68k/stand/bootsd/version.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD: src/sys/arch/mvme68k/stand/bootsd/version.c,v 1.4 1996/04/28 10:48:19 deraadt Exp $ */
+/*	$OpenBSD: src/sys/arch/mvme68k/stand/bootsd/version.c,v 1.5 1996/05/16 02:46:57 chuck Exp $ */
 
 /*
  * make a random change to this file when you want the bootblock
- * revision to increase. like change this x to a y, or something.
+ * revision to increase. like change this x to a z, or something.
  */
 
-char *version = "$Revision: 1.3 $";
+char *version = "$Revision: 1.4 $";
