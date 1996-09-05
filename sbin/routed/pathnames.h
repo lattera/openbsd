ac95bdf2a809cc04f233b9b70415f89fb45425ee
mickey <mickey>
1996-09-05 13:59:01 +0000

    new routed from SGI. rip1, rip2, icmp, rdisc.

--- sbin/routed/pathnames.h
+++ sbin/routed/pathnames.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pathnames.h,v 1.6 1995/03/18 15:00:37 cgd Exp $	*/
+/*	$OpenBSD	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -33,8 +33,16 @@
  * SUCH DAMAGE.
  *
  *	@(#)pathnames.h	8.1 (Berkeley) 6/5/93
+ *
  */
 
 #include <paths.h>
 
 #define	_PATH_GATEWAYS	"/etc/gateways"
+
+/* All remotely requested trace files must either start with this prefix
+ * or be the same as the tracefile specified when the daemon was started.
+ * If this is a directory, routed will create log files in it.  That
+ * might be a security problem.
+ */
+#define _PATH_TRACE	"/tmp/routed.log"
