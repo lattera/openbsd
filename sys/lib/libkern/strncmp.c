049e2dd539d8701d908c82b6fc6d94edd7d8dfce
niklas <niklas>
1996-12-08 14:25:52 +0000

    -Wcast-qual happiness

--- sys/lib/libkern/strncmp.c
+++ sys/lib/libkern/strncmp.c
@@ -33,7 +33,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)strncmp.c	5.6 (Berkeley) 1/26/91";*/
-static char *rcsid = "$Id: strncmp.c,v 1.1.1.1 1995/10/18 08:52:50 deraadt Exp $";
+static char *rcsid = "$Id: strncmp.c,v 1.2 1996/05/01 15:18:52 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #ifndef _KERNEL
@@ -52,7 +52,7 @@ strncmp(s1, s2, n)
 		return (0);
 	do {
 		if (*s1 != *s2++)
-			return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+			return (*(const u_char *)s1 - *(const u_char *)--s2);
 		if (*s1++ == 0)
 			break;
 	} while (--n != 0);
