42dd3fb4935c773ea772d73aadd456010eadd459
deraadt <deraadt>
1996-12-16 17:09:06 +0000

    strncat related oflows

--- kerberosIV/krb/str2key.c
+++ kerberosIV/krb/str2key.c
@@ -1,7 +1,7 @@
 /*
  * This software may now be redistributed outside the US.
  *
- * $Source: /usr/src/kerberosIV/lib/krb/RCS/str2key.c,v $
+ * $Source: /cvs/src/kerberosIV/krb/str2key.c,v $
  *
  * $Locker:  $
  */
@@ -90,10 +90,12 @@ afs_transarc_StringToKey (str, cell, key)
     char password[BUFSIZ];
     int  passlen;
 
-    strncpy (password, str, sizeof(password));
+    strncpy (password, str, sizeof(password)-1);
+    password[sizeof(password)-1] = '\0';
     if ((passlen = strlen (password)) < sizeof(password)-1)
         strncat (password, cell, sizeof(password)-passlen);
-    if ((passlen = strlen(password)) > sizeof(password)) passlen = sizeof(password);
+    if ((passlen = strlen(password)) > sizeof(password))
+	passlen = sizeof(password);
 
     bcopy ("kerberos", &ivec, 8);
     bcopy ("kerberos", &temp_key, 8);
