3fc3ca2b0b5206a1e8beecbf6157e998130a12c5
downsj <downsj>
1997-03-12 14:51:59 +0000

    Remove %INDOT%.

--- usr.sbin/named/restart/named.restart.sh
+++ usr.sbin/named/restart/named.restart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -
 #
-#	$OpenBSD: src/usr.sbin/named/restart/Attic/named.restart.sh,v 1.2 1997/03/12 10:42:53 downsj Exp $
+#	$OpenBSD: src/usr.sbin/named/restart/Attic/named.restart.sh,v 1.3 1997/03/12 14:51:59 downsj Exp $
 #	from named.restart	5.4 (Berkeley) 6/27/89
-#	$Id: named.restart.sh,v 8.1 1994/12/15 06:24:14 vixie Exp $
+#	$Id: named.restart.sh,v 1.2 1997/03/12 10:42:53 downsj Exp $
 #
 
-exec %DESTSBIN%/%INDOT%ndc restart
+exec %DESTSBIN%/ndc restart
