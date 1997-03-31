eacda0a1c361dae80b58e070b1482c56025f952a
downsj <downsj>
1997-03-31 00:24:17 +0000

    Modernize OpenBSD/mvme68k to match various changes in m68k and hp300 code.
    Compiles, but untested for lack of hardware.

--- sys/arch/mvme68k/include/float.h
+++ sys/arch/mvme68k/include/float.h
@@ -1,8 +1,3 @@
-/*	$OpenBSD: src/sys/arch/mvme68k/include/Attic/float.h,v 1.3 1996/04/28 10:56:18 deraadt Exp $ */
-
-#ifndef _MACHINE_FLOAT_H_
-#define _MACHINE_FLOAT_H_
+/*	$OpenBSD: src/sys/arch/mvme68k/include/Attic/float.h,v 1.4 1997/03/31 00:24:01 downsj Exp $ */
 
 #include <m68k/float.h>
-
-#endif
