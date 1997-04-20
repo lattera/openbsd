f04d0079d33f4cbeccd9d2abbbba321a787dd9d0
downsj <downsj>
1997-04-20 23:38:18 +0000

    Move hp300 rdsetroot.c to top level common directory, the one copy should be used by all ports.

--- distrib/common/rdsetroot.c
+++ distrib/common/rdsetroot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/distrib/hp300/common/Attic/rdsetroot.c,v 1.1 1997/02/16 18:19:59 downsj Exp $	*/
+/*	$OpenBSD: src/distrib/common/Attic/rdsetroot.c,v 1.1 1997/04/20 23:38:17 downsj Exp $	*/
 /*	$NetBSD: rdsetroot.c,v 1.2 1995/10/13 16:38:39 gwr Exp $	*/
 
 /*
