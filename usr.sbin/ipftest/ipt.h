bfd5938c330d3628b29cc80533578826fd423370
kstailey <kstailey>
1997-06-23 17:08:04 +0000

    fix tag

--- usr.sbin/ipftest/ipt.h
+++ usr.sbin/ipftest/ipt.h
@@ -1,10 +1,11 @@
+/*	$OpenBSD: src/usr.sbin/ipftest/Attic/ipt.h,v 1.4 1997/06/23 17:08:04 kstailey Exp $	*/
 /*
  * (C)opyright 1993,1994,1995 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
  * to the original author and the contributors.
- * $Id: ipt.h,v 2.0.1.1 1997/01/09 15:14:44 darrenr Exp $
+ * $DRId: ipt.h,v 2.0.1.1 1997/01/09 15:14:44 darrenr Exp $
  */
 
 #include <fcntl.h>
