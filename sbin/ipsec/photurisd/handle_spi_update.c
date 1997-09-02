8639bb0a5e9304cd84f6341fbb5e04d3972e5901
provos <provos>
1997-09-02 17:26:50 +0000

    including changes between drafts-14-16. update cookie and counters correctly after receiving a resource limit message.

--- sbin/ipsec/photurisd/handle_spi_update.c
+++ sbin/ipsec/photurisd/handle_spi_update.c
@@ -34,7 +34,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: handle_spi_update.c,v 1.2 1997/07/19 12:07:50 provos Exp $";
+static char rcsid[] = "$Id: handle_spi_update.c,v 1.3 1997/07/23 12:28:50 provos Exp $";
 #endif
 
 #include <stdio.h>
@@ -49,6 +49,7 @@ static char rcsid[] = "$Id: handle_spi_update.c,v 1.2 1997/07/19 12:07:50 provos
 #include "packet.h"
 #include "encrypt.h"
 #include "validity.h"
+#include "attributes.h"
 #include "secrets.h"
 #include "schedule.h"
 #include "scheme.h"
