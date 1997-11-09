776378f08d4901c0634bce6709cea8348577cc85
niklas <niklas>
1997-11-09 23:05:19 +0000

    New endian.h design.  All architectures now use a common file for the bulk part.  Some ports can optimize the swap operations.  This also means the general API is extended with swap16, swap32, htobe16, htobe32, betoh16, betoh32, htole16, htole32, letoh16 and letoh32.

--- sys/arch/mac68k/include/endian.h
+++ sys/arch/mac68k/include/endian.h
@@ -1,4 +1,9 @@
-/*	$OpenBSD: src/sys/arch/mac68k/include/Attic/endian.h,v 1.2 1996/05/26 18:35:48 briggs Exp $	*/
+/*	$OpenBSD: src/sys/arch/mac68k/include/Attic/endian.h,v 1.3 1997/11/09 23:05:09 niklas Exp $	*/
 /*	$NetBSD: endian.h,v 1.5 1994/10/26 08:46:28 cgd Exp $	*/
 
+#ifndef _MAC68K_ENDIAN_H_
+#define _MAC68K_ENDIAN_H_
+
 #include <m68k/endian.h>
+
+#endif /* _MAC68K_ENDIAN_H */
