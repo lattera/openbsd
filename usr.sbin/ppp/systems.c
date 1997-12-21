9804c07bf236c53fbfd93b7ceae9e783ad74cd15
brian <brian>
1997-12-21 02:11:29 +0000

    Index: systems.c =================================================================== RCS file: /home/ncvs/src/usr.sbin/ppp/systems.c,v retrieving revision 1.31 diff -u -r1.31 systems.c --- systems.c	1997/12/17 21:22:01	1.31 +++ systems.c	1997/12/21 01:35:58 @@ -288,6 +288,8 @@            if (issep(*cp)) {  	    n = strspn(cp, " \t");  	    cp += n; +	    if (*cp == '#') +	      continue;              len = strlen(cp);              if (!len)                continue;

--- usr.sbin/ppp/systems.c
+++ usr.sbin/ppp/systems.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: systems.c,v 1.3 1997/12/17 21:17:48 brian Exp $
+ * $Id: systems.c,v 1.4 1997/12/17 21:18:07 brian Exp $
  *
  *  TODO:
  */
@@ -288,6 +288,8 @@ ReadSystem(const char *name, const char *file, int doexec)
           if (issep(*cp)) {
 	    n = strspn(cp, " \t");
 	    cp += n;
+	    if (*cp == '#')
+	      continue;
             len = strlen(cp);
             if (!len)
               continue;
