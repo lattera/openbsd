82a6f71da6d09db2a81f89bbdd7d9802a442f89d
brian <brian>
1997-12-28 21:54:25 +0000

    Don't expect select() to adjust the passed time when it's interrupted with a SIGALRM.  In fact, select() sets the passed time to zero, making the previous implementation terminate always after 1/10th of a second !
    Also, deal with someone changing the clock while we're
    sleeping (and restart the whole sleep).
    
    Dangers pointed out by: Theo de Raadt <deraadt@cvs.openbsd.org>

--- usr.sbin/ppp/timer.h
+++ usr.sbin/ppp/timer.h
@@ -15,7 +15,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: timer.h,v 1.3 1997/11/22 03:37:52 brian Exp $
+ * $Id: timer.h,v 1.1.1.1 1997/11/23 20:27:36 brian Exp $
  *
  *	TODO:
  */
@@ -46,6 +46,6 @@ extern void TermTimerService(void);
 extern void ShowTimers(void);
 
 #ifdef SIGALRM
-extern u_int nointr_sleep(u_int);
+extern void nointr_sleep(u_int);
 extern void nointr_usleep(u_int);
 #endif
