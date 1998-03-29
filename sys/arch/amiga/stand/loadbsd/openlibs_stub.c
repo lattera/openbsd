27fa7f4a3309c2b2bd2fe815cd34ce824d5b56e6
espie <espie>
1998-03-29 22:24:54 +0000

    Updated to compile with recent AmigaOS Geek Gadgets toolchain, plus clean-up.

---
+++ sys/arch/amiga/stand/loadbsd/openlibs_stub.c
@@ -0,0 +1,6 @@
+/*?$OpenBSD: src/sys/arch/amiga/stand/loadbsd/Attic/openlibs_stub.c,v 1.1 1998/03/29 22:24:53 espie Exp $?*/
+
+void open_libraries()
+{
+/* relies on libnix auto-open instead */
+}
---
+++ sys/arch/amiga/stand/loadbsd/vers_ixemul.c
@@ -0,0 +1,3 @@
+/*?$OpenBSD: src/sys/arch/amiga/stand/loadbsd/Attic/vers_ixemul.c,v 1.1 1998/03/29 22:24:54 espie Exp $?*/
+
+const char _version[] = "$VER: LoadBSD (ixemul) 2.15.2 (OpenBSD 28.03.98)";
