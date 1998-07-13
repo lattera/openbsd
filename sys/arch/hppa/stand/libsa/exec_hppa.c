1c72d38af303694f9e91dbedc7128167cf46ac4f
mickey <mickey>
1998-07-13 03:52:12 +0000

    since i blew up prev commit msg, here it is: upgrade to new coming stand/exec framework

--- sys/arch/hppa/stand/libsa/exec_hppa.c
+++ sys/arch/hppa/stand/libsa/exec_hppa.c
@@ -1,5 +1,4 @@
-/*	$OpenBSD: src/sys/arch/hppa/stand/libsa/Attic/exec_hppa.c,v 1.2 1998/07/13 03:35:59 mickey Exp $	*/
-/*	$NOWHERE: exec_hppa.c,v 2.2 1998/06/22 19:34:46 mickey Exp $	*/
+/*	$OpenBSD: src/sys/arch/hppa/stand/libsa/Attic/exec_hppa.c,v 1.3 1998/07/13 03:52:11 mickey Exp $	*/
 
 /*
  * Copyright (c) 1998 Michael Shalayeff
