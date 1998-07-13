eb6de75c0c8674a5e11907f9aec4801ea6f856de
art <art>
1998-07-13 20:08:55 +0000

    use sizeof() instead of MAXHOSTNAMELEN in gethostname

--- kerberosIV/kinit/kinit.c
+++ kerberosIV/kinit/kinit.c
@@ -1,5 +1,5 @@
-/*	$OpenBSD: src/kerberosIV/kinit/Attic/kinit.c,v 1.3 1997/12/12 05:30:16 art Exp $	*/
-/*	$Id: kinit.c,v 1.2 1997/11/28 12:48:47 art Exp $	*/
+/*	$OpenBSD: src/kerberosIV/kinit/Attic/kinit.c,v 1.4 1998/07/13 20:08:55 art Exp $	*/
+/*	$Id: kinit.c,v 1.3 1997/12/12 05:30:16 art Exp $	*/
 /*      $KTH: kinit.c,v 1.15 1997/03/30 18:58:46 assar Exp $    */
 
 
@@ -112,7 +112,7 @@ main(int argc, char **argv)
 	iflag = rflag = 1;
 	username = NULL;
     }
-    if (gethostname(buf, MAXHOSTNAMELEN)) 
+    if (gethostname(buf, sizeof(buf))) 
 	errx(1, "gethostname failed");
     printf("%s (%s)\n", ORGANIZATION, buf);
     if (username) {
