ad3835dfc1a023720ff3483d30ab2e5ea6add609
smurph <smurph>
1998-08-18 04:21:27 +0000

    fixed passing of bugargs to kernel.

---
+++ sys/arch/mvme88k/stand/bootst/version.c
@@ -0,0 +1,8 @@
+/*	$OpenBSD: src/sys/arch/mvme88k/stand/bootst/version.c,v 1.1 1998/08/18 04:21:27 smurph Exp $ */
+
+/*
+ * make a random change to this file when you want the bootblock
+ * revision to increase. like change this q to an x, or something.
+ */
+
+char *version = "$Revision: 1.2 $";
