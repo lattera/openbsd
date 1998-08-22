fad78a30a5b4cf6314bbe2512ba7db12598d55e4
smurph <smurph>
1998-08-22 07:02:07 +0000

    Part of remove to clean up stand.

--- sys/arch/mvme88k/stand/openbsd/bootsd/version.c
+++
@@ -1,8 +0,0 @@
-/*	$Id: version.c,v 1.3 1995/12/06 10:54:56 deraadt Exp $ */
-
-/*
- * make a random change to this file when you want the bootblock
- * revision to increase. like change this x to a y, or something.
- */
-
-char *version = "$Revision: 1.3 $";
--- sys/arch/mvme88k/stand/openbsd/netboot/version.c
+++
@@ -1,9 +0,0 @@
-/*	$Id: version.c,v 1.2 1995/11/07 08:51:04 deraadt Exp $ */
-
-/*
- *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
- *
- *	1.1
- */
-
-char *version = "$Revision: 1.2 $";
