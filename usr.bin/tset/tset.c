0986ba12bbf21918e075b93eb5dcda4a50231254
millert <millert>
1998-11-17 03:25:15 +0000

    preserve ncruses module id

--- usr.bin/tset/tset.c
+++ usr.bin/tset/tset.c
@@ -103,7 +103,7 @@ char *ttyname(int fd);
 #include <curses.h>	/* for bool typedef */
 #include <dump_entry.h>
 
-MODULE_ID("$Id: tset.c,v 0.33 1998/04/04 19:09:17 juergen Exp $")
+MODULE_ID("$From: tset.c,v 0.34 1998/09/05 22:02:19 tom Exp $")
 
 extern char **environ;
 
