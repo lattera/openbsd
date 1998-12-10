0d5af39394381725559cf4dd12f08af3e607cb13
brian <brian>
1998-12-10 18:40:56 +0000

    Boy, this was tricky to find:   Remove any dial timer that might be hanging around at   datalink_Destroy() time.  This timer may be left running   after the link is closed (making sure it's not automatically   opened again too soon).

--- usr.sbin/ppp/ppp/datalink.c
+++ usr.sbin/ppp/ppp/datalink.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: datalink.c,v 1.1 1998/08/31 00:22:19 brian Exp $
+ *	$Id: datalink.c,v 1.2 1998/10/29 02:21:44 brian Exp $
  */
 
 #include <sys/types.h>
@@ -811,6 +811,7 @@ datalink_Destroy(struct datalink *dl)
     }
   }
 
+  timer_Stop(&dl->dial_timer);
   result = dl->next;
   modem_Destroy(dl->physical);
   free(dl->name);
