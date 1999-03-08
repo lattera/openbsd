ee0174f6186158df4d5149a32aff970f80e81dbb
brian <brian>
1999-03-08 01:40:23 +0000

    Add proxying & transparent proxying support (``alias proxy''). Add PPTP packet aliasing (GRE fixed address) support (``alias pptp''). Add a 0 to the front of the mode to signify octal. Thanks to: Charles Mott <cmott@srv.net> & Dru Nelson <dnelson@redwoodsoft.com>

--- usr.sbin/ppp/ppp/alias_cmd.h
+++ usr.sbin/ppp/ppp/alias_cmd.h
@@ -2,10 +2,12 @@
  * The code in this file was written by Eivind Eklund <perhaps@yes.no>,
  * who places it in the public domain without restriction.
  *
- *	$Id: alias_cmd.h,v 1.8 1998/05/21 21:43:45 brian Exp $
+ *	$Id: alias_cmd.h,v 1.10 1999/03/07 18:13:44 brian Exp $
  */
 
 struct cmdargs;
 
 extern int alias_RedirectPort(struct cmdargs const *);
 extern int alias_RedirectAddr(struct cmdargs const *);
+extern int alias_ProxyRule(struct cmdargs const *);
+extern int alias_Pptp(struct cmdargs const *);
