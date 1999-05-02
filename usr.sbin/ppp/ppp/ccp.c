56d0a4bf25c16ab73fd0eb0ac904e8d73ed6e612
brian <brian>
1999-05-02 14:34:02 +0000

    Handle receiving more than one acceptable CCP REQ during one negotiation session without REJecting everything from the second REQuest.

--- usr.sbin/ppp/ppp/ccp.c
+++ usr.sbin/ppp/ppp/ccp.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Id: ccp.c,v 1.4 1999/03/11 01:50:04 brian Exp $
+ * $Id: ccp.c,v 1.5 1999/03/31 14:22:10 brian Exp $
  *
  *	TODO:
  *		o Support other compression protocols
@@ -411,9 +411,11 @@ CcpDecodeConfig(struct fsm *fp, u_char *cp, int plen, int mode_type,
 {
   /* Deal with incoming data */
   struct ccp *ccp = fsm2ccp(fp);
-  int type, length;
-  int f;
+  int type, length, f;
   const char *end;
+
+  if (mode_type == MODE_REQ)
+    ccp->in.algorithm = -1;	/* In case we've received two REQs in a row */
 
   while (plen >= sizeof(struct fsmconfig)) {
     type = *cp;
