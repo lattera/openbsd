1404fc5b135466d98d8a87f5e641782062e2716c
millert <millert>
1999-05-09 16:34:10 +0000

    o kill colon-separated paths in *libexp, they confuse too many modules o kill MakeMaker hack (no longer needed) o define APPLLIB_EXP to be /usr/local/libdata/perl5 to let users   override standard modules if they want
    The perl search path in @INC is now:
    /usr/local/libdata/perl5
    /usr/libdata/perl5/i386-openbsd/5.00503
    /usr/libdata/perl5
    /usr/local/libdata/perl5/site_perl/i386-openbsd
    /usr/local/libdata/perl5/site_perl
    .

--- gnu/usr.bin/perl/lib/ExtUtils/MM_Unix.pm
+++ gnu/usr.bin/perl/lib/ExtUtils/MM_Unix.pm
@@ -126,7 +126,6 @@ sub catfile {
     return $self->canonpath($file) unless @_;
     my $dir = $self->catdir(@_);
     for ($dir) {
-	$_ =~ s/:.*$//;
 	$_ .= "/" unless substr($_,length($_)-1,1) eq "/";
     }
     return $self->canonpath($dir.$file);
@@ -1604,12 +1603,7 @@ from the perl source tree.
 	# we should also consider $ENV{PERL5LIB} here
 	$self->{PERL_LIB}     ||= $Config::Config{privlibexp};
 	$self->{PERL_ARCHLIB} ||= $Config::Config{archlibexp};
-	foreach (split(':', $self->{PERL_ARCHLIB})) {
-	    last if (-d ($self->{PERL_INC} = $self->catdir($_, "CORE")));
-	}
-	# use the primary path compenents of privlibexp and archlibexp
-	$self->{PERL_LIB} =~ s/:.*//;
-	$self->{PERL_ARCHLIB} =~ s/:.*//;
+	$self->{PERL_INC}     = $self->catdir("$self->{PERL_ARCHLIB}","CORE"); # wild guess for now
 	my $perl_h;
 	unless (-f ($perl_h = $self->catfile($self->{PERL_INC},"perl.h"))){
 	    die qq{
