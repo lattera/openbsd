7e66a5c713fd4f09566d8474da23e97ed81ee540
deraadt <deraadt>
1999-11-22 05:13:39 +0000

    when following SEE-ALSO in man mode, would SIGSEGV when cursor is at start of line

--- gnu/usr.bin/texinfo/info/session.c
+++ gnu/usr.bin/texinfo/info/session.c
@@ -1,5 +1,5 @@
 /* session.c -- The user windowing interface to Info.
-   $Id: session.c,v 1.13 1998/02/22 22:38:30 karl Exp $
+   $Id: session.c,v 1.2 1999/01/11 16:38:09 espie Exp $
 
    Copyright (C) 1993, 96, 97 Free Software Foundation, Inc.
 
@@ -1912,6 +1912,8 @@ info_menu_or_ref_item (window, count, key, builder, ask_p)
                       which = closest;
                   }
 
+                if (which < 0)
+                  which = 0;
                 defentry = (REFERENCE *)xmalloc (sizeof (REFERENCE));
                 defentry->label = xstrdup (refs[which]->label);
                 defentry->filename = refs[which]->filename;
