f519025d65fffa158b242da4dc911aeb01d563ff
jason <jason>
1999-12-07 22:01:33 +0000

    Merge with NetBSD: o move common support functions for phy drivers from mii.c to mii_physubr.c, 	so that they are not includes if no PHY is configured o Clean up the code that adds media a little, and make media selection 	table-driven in preparation for some other changes to be made. o Don't add any loopback versions of media, for now. o Add mii_down(), which is used by MAC drivers to inform PHYs that the 	interface is now down.  PHYs use this to cancel pending asynchronous 	operations. o Add OUI for Enable Semiconductor. o New Driver for TDK TSC78Q2120 PHY

--- sys/dev/mii/tqphy.c
+++ sys/dev/mii/tqphy.c
@@ -1,7 +1,7 @@
-/*	$OpenBSD: src/sys/dev/mii/icsphy.c,v 1.5 1999/12/07 22:01:30 jason Exp $	*/
-/*	$NetBSD: icsphy.c,v 1.8.6.1 1999/04/23 15:40:56 perry Exp $	*/
+/*	$OpenBSD: src/sys/dev/mii/tqphy.c,v 1.1 1999/12/07 22:01:33 jason Exp $	*/
+/*	$NetBSD: tqphy.c,v 1.4 1999/11/12 18:13:01 thorpej Exp $	*/
 
-/*-
+/*
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
@@ -37,7 +37,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 /*
  * Copyright (c) 1997 Manuel Bouyer.  All rights reserved.
  *
@@ -68,8 +68,9 @@
  */
 
 /*
- * driver for Integrated Circuit Systems' ICS1890 ethernet 10/100 PHY
- * datasheet from www.icst.com
+ * TDK TSC78Q2120 PHY driver
+ *
+ * Documentation available at http://www.tsc.tdk.com/lan/78Q2120.pdf .
  */
 
 #include <sys/param.h>
@@ -86,41 +87,42 @@
 #include <dev/mii/miivar.h>
 #include <dev/mii/miidevs.h>
 
-#include <dev/mii/icsphyreg.h>
+#include <dev/mii/tqphyreg.h>
 
-int	icsphymatch __P((struct device *, void *, void *));
-void	icsphyattach __P((struct device *, struct device *, void *));
-int	icsphydetach __P((struct device *, int));
+int	tqphymatch __P((struct device *, void *, void *));
+void	tqphyattach __P((struct device *, struct device *, void *));
 
-struct cfattach icsphy_ca = {
-	sizeof(struct mii_softc), icsphymatch, icsphyattach, icsphydetach
+struct cfattach tqphy_ca = {
+	sizeof(struct mii_softc), tqphymatch, tqphyattach
 };
 
-struct cfdriver icsphy_cd = {
-	NULL, "icsphy", DV_DULL
+struct cfdriver tqphy_cd = {
+	NULL, "tqphy", DV_DULL
 };
 
-int	icsphy_service __P((struct mii_softc *, struct mii_data *, int));
-void	icsphy_reset __P((struct mii_softc *));
-void	icsphy_status __P((struct mii_softc *));
+int	tqphy_service __P((struct mii_softc *, struct mii_data *, int));
+void	tqphy_status __P((struct mii_softc *));
 
 int
-icsphymatch(parent, match, aux)
+tqphymatch(parent, match, aux)
 	struct device *parent;
 	void *match;
 	void *aux;
 {
 	struct mii_attach_args *ma = aux;
 
-	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_xxICS &&
-	    MII_MODEL(ma->mii_id2) == MII_MODEL_xxICS_1890)
-		return (10);
+	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_TSC)
+		switch MII_MODEL(ma->mii_id2) {
+		case MII_MODEL_TSC_78Q2120:
+		/* case MII_MODEL_TSC_78Q2121: */
+			return (10);
+	}
 
 	return (0);
 }
 
 void
-icsphyattach(parent, self, aux)
+tqphyattach(parent, self, aux)
 	struct device *parent, *self;
 	void *aux;
 {
@@ -128,32 +130,33 @@ icsphyattach(parent, self, aux)
 	struct mii_attach_args *ma = aux;
 	struct mii_data *mii = ma->mii_data;
 
-	printf(": %s, rev. %d\n", MII_STR_xxICS_1890,
+	printf(": %s, rev. %d\n", MII_STR_TSC_78Q2120,
 	    MII_REV(ma->mii_id2));
 
 	sc->mii_inst = mii->mii_instance;
 	sc->mii_phy = ma->mii_phyno;
-	sc->mii_service = icsphy_service;
+	sc->mii_service = tqphy_service;
 	sc->mii_pdata = mii;
 
-	icsphy_reset(sc);
+	/*
+	 * Apparently, we can't do loopback on this PHY.
+	 */
+	sc->mii_flags |= MIIF_NOLOOP;
+
+	mii_phy_reset(sc);
 
 	sc->mii_capabilities =
 	    PHY_READ(sc, MII_BMSR) & ma->mii_capmask;
-	if (sc->mii_capabilities & BMSR_MEDIAMASK)
+	printf("%s: ", sc->mii_dev.dv_xname);
+	if ((sc->mii_capabilities & BMSR_MEDIAMASK) == 0)
+		printf("no media present");
+	else
 		mii_add_media(sc);
+	printf("\n");
 }
 
 int
-icsphydetach(dev, flags)
-	struct device *dev;
-	int flags;
-{
-	return (0);
-}
-
-int
-icsphy_service(sc, mii, cmd)
+tqphy_service(sc, mii, cmd)
 	struct mii_softc *sc;
 	struct mii_data *mii;
 	int cmd;
@@ -221,9 +224,25 @@ icsphy_service(sc, mii, cmd)
 			return (0);
 
 		/*
-		 * The ICS1890's autonegotiation doesn't need to be
-		 * kicked; it continues in the background.
+		 * Check to see if we have link.  If we do, we don't
+		 * need to restart the autonegotiation process.  Read
+		 * the BMSR twice in case it's latched.
 		 */
+		reg = PHY_READ(sc, MII_BMSR) |
+		    PHY_READ(sc, MII_BMSR);
+		if (reg & BMSR_LINK)
+			return (0);
+
+		/*
+		 * Only retry autonegotiation every 5 seconds.
+		 */
+		if (++sc->mii_ticks != 5)
+			return (0);
+
+		sc->mii_ticks = 0;
+		mii_phy_reset(sc);
+		if (mii_phy_auto(sc, 0) == EJUSTRETURN)
+			return (0);
 		break;
 
 	case MII_DOWN:
@@ -232,7 +251,7 @@ icsphy_service(sc, mii, cmd)
 	}
 
 	/* Update the media status. */
-	icsphy_status(sc);
+	tqphy_status(sc);
 
 	/* Callback if something changed. */
 	if (sc->mii_active != mii->mii_media_active || cmd == MII_MEDIACHG) {
@@ -243,58 +262,44 @@ icsphy_service(sc, mii, cmd)
 }
 
 void
-icsphy_status(sc)
+tqphy_status(sc)
 	struct mii_softc *sc;
 {
 	struct mii_data *mii = sc->mii_pdata;
 	struct ifmedia_entry *ife = mii->mii_media.ifm_cur;
-	int bmcr, qpr;
+	int bmsr, bmcr, diag;
 
 	mii->mii_media_status = IFM_AVALID;
 	mii->mii_media_active = IFM_ETHER;
 
-	/*
-	 * Don't get link from the BMSR.  It's available in the QPR,
-	 * and we have to read it twice to unlatch it anyhow.  This
-	 * gives us fewer register reads.
-	 */
-	qpr = PHY_READ(sc, MII_ICSPHY_QPR);		/* unlatch */
-	qpr = PHY_READ(sc, MII_ICSPHY_QPR);		/* real value */
-
-	if (qpr & QPR_LINK)
+	bmsr = PHY_READ(sc, MII_BMSR) |
+	    PHY_READ(sc, MII_BMSR);
+	if (bmsr & BMSR_LINK)
 		mii->mii_media_status |= IFM_ACTIVE;
-
+ 
 	bmcr = PHY_READ(sc, MII_BMCR);
 	if (bmcr & BMCR_ISO) {
 		mii->mii_media_active |= IFM_NONE;
 		mii->mii_media_status = 0;
 		return;
 	}
-
+ 
 	if (bmcr & BMCR_LOOP)
 		mii->mii_media_active |= IFM_LOOP;
-
+ 
 	if (bmcr & BMCR_AUTOEN) {
-		if ((qpr & QPR_ACOMP) == 0) {
+		if ((bmsr & BMSR_ACOMP) == 0) { 
 			/* Erg, still trying, I guess... */
 			mii->mii_media_active |= IFM_NONE;
 			return;
 		}
-		if (qpr & QPR_SPEED)
+		diag = PHY_READ(sc, MII_TQPHY_DIAG);
+		if (diag & DIAG_RATE)
 			mii->mii_media_active |= IFM_100_TX;
 		else
 			mii->mii_media_active |= IFM_10_T;
-		if (qpr & QPR_FDX)
+		if (diag & DIAG_DPLX)
 			mii->mii_media_active |= IFM_FDX;
-	} else
+	} else 
 		mii->mii_media_active = ife->ifm_media;
-}
-
-void
-icsphy_reset(sc)
-	struct mii_softc *sc;
-{
-
-	mii_phy_reset(sc);
-	PHY_WRITE(sc, MII_ICSPHY_ECR2, ECR2_10TPROT|ECR2_Q10T);
 }
