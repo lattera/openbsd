34111cd1bb625a51f4a5a972ae31062f67b7c76b
deraadt <deraadt>
2000-01-08 05:28:08 +0000

    pull in arc4random() prototype

--- sys/netinet6/frag6.c
+++ sys/netinet6/frag6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/netinet6/frag6.c,v 1.3 2000/01/08 04:49:22 deraadt Exp $	*/
+/*	$OpenBSD: src/sys/netinet6/frag6.c,v 1.4 2000/01/08 05:28:08 deraadt Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -51,6 +51,8 @@
 #include <netinet6/icmp6.h>
 
 #include <net/net_osdep.h>
+
+#include <dev/rndvar.h>
 
 /*
  * Define it to get a correct behavior on per-interface statistics.
