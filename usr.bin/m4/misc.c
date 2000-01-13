79b31e3f8f4ece98befac5eec5d586cb7ebca03a
espie <espie>
2000-01-13 17:35:10 +0000

    Record 50 levels of open parenthesis, open quotes, to give a post-mortem. - no need to record more, as this is just for diagnosis purpose. This doesn't affect m4's main engine. - don't free file names as a compromise: the only other options would be to ref count them (not worth the complexity) or dup them systematically (ick).

--- usr.bin/m4/misc.c
+++ usr.bin/m4/misc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.bin/m4/misc.c,v 1.15 2000/01/12 17:49:53 espie Exp $	*/
+/*	$OpenBSD: src/usr.bin/m4/misc.c,v 1.16 2000/01/13 17:35:10 espie Exp $	*/
 /*	$NetBSD: misc.c,v 1.6 1995/09/28 05:37:41 tls Exp $	*/
 
 /*
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)misc.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: src/usr.bin/m4/misc.c,v 1.15 2000/01/12 17:49:53 espie Exp $";
+static char rcsid[] = "$OpenBSD: src/usr.bin/m4/misc.c,v 1.16 2000/01/13 17:35:10 espie Exp $";
 #endif
 #endif /* not lint */
 
@@ -324,5 +324,8 @@ release_input(f)
 {
 	if (f->file != stdin)
 	    fclose(f->file);
-	free(f->name);
+	/*
+	 * XXX can't free filename, as there might still be 
+	 * error information pointing to it.
+	 */
 }
