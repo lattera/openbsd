edd0510425faacd8866d153dd0b20ee7230738ea
ericj <ericj>
2000-03-26 05:24:25 +0000

    correct usage() return value

--- usr.sbin/tcpdump/tcpdump.c
+++ usr.sbin/tcpdump/tcpdump.c
@@ -24,7 +24,7 @@ static const char copyright[] =
     "@(#) Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997\n\
 The Regents of the University of California.  All rights reserved.\n";
 static const char rcsid[] =
-    "@(#) $Header: /cvs/src/usr.sbin/tcpdump/tcpdump.c,v 1.14 1999/09/16 20:58:48 brad Exp $ (LBL)";
+    "@(#) $Header: /cvs/src/usr.sbin/tcpdump/tcpdump.c,v 1.15 2000/01/16 13:36:36 jakob Exp $ (LBL)";
 #endif
 
 /*
@@ -517,5 +517,5 @@ usage(void)
 "\t\t[ -i interface ] [ -r file ] [ -s snaplen ]\n");
 	(void)fprintf(stderr,
 "\t\t[ -T type ] [ -w file ] [ expression ]\n");
-	exit(-1);
+	exit(1);
 }
