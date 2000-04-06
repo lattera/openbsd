df32fb4e17793a82932798821a49b352287d3b90
millert <millert>
2000-04-06 16:11:10 +0000

    virgin perl 5.6.0

--- gnu/usr.bin/perl/ext/ODBM_File/hints/sco.pl
+++ gnu/usr.bin/perl/ext/ODBM_File/hints/sco.pl
@@ -1,4 +1,4 @@
-# Some versions of SCO contain a broken -ldbm library that is missing
-# dbmclose.  Some of those might have a fixed library installed as
-# -ldbm.nfs.
-$self->{LIBS} = ['-ldbm.nfs', '-ldbm'];
+# SCO ODT 3.2v4.2 has a -ldbm library that is missing dbmclose.  
+# This system should have a complete library installed as -ldbm.nfs which
+# should be used instead (Probably need the networking product add-on)
+$self->{LIBS} = ['-lndbm',-e "/usr/lib/libdbm.nfs.a"?'-ldbm.nfs':'-ldbm'];
