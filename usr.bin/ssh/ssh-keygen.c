17efea872bf86c1a35faea25f7bfd8d2a2bd9ab4
deraadt <deraadt>
2000-05-03 02:52:56 +0000

    simplify usage

--- usr.bin/ssh/ssh-keygen.c
+++ usr.bin/ssh/ssh-keygen.c
@@ -7,7 +7,7 @@
  */
 
 #include "includes.h"
-RCSID("$Id: ssh-keygen.c,v 1.22 2000/05/01 08:20:56 hugh Exp $");
+RCSID("$Id: ssh-keygen.c,v 1.23 2000/05/02 22:18:04 markus Exp $");
 
 #include <openssl/evp.h>
 #include <openssl/pem.h>
@@ -488,8 +488,7 @@ do_change_comment(struct passwd *pw)
 void
 usage(void)
 {
-	printf("ssh-keygen version %s\n", SSH_VERSION);
-	printf("Usage: %s [-b bits] [-c] [-d] [-f file] [-l] [-p] [-q] [-x] [-y] [-C comment] [-N new-pass] [-P pass] [-X]\n", __progname);
+	printf("Usage: %s [-lpqxXydc] [-b bits] [-f file] [-C comment] [-N new-pass] [-P pass]\n", __progname);
 	exit(1);
 }
 
