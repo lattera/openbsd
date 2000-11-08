43c8e2030b4f05f1d644a2885a3f865eb340c687
art <art>
2000-11-08 22:05:51 +0000

    Zap dead files.

--- sys/arch/alpha/alpha/dec_axppci_33.h
+++
@@ -1,1 +0,0 @@
-/*	$OpenBSD: src/sys/arch/alpha/alpha/Attic/dec_axppci_33.h,v 1.5 1997/01/24 19:56:28 niklas Exp $	*/
--- sys/arch/alpha/alpha/dec_kn20aa.h
+++
@@ -1,1 +0,0 @@
-/*	$OpenBSD: src/sys/arch/alpha/alpha/Attic/dec_kn20aa.h,v 1.5 1997/01/24 19:56:31 niklas Exp $	*/
