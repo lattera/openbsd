7d3da4a09fe82871a79239ea7372f4be2d3af982
fgsch <fgsch>
2001-01-17 06:11:15 +0000

    empty line at the top.

--- usr.sbin/ipsend/iptest/iptest.c
+++ usr.sbin/ipsend/iptest/iptest.c
@@ -1,5 +1,4 @@
-
-/*	$OpenBSD: src/usr.sbin/ipsend/iptest/Attic/iptest.c,v 1.5 2001/01/17 06:01:27 fgsch Exp $	*/
+/*	$OpenBSD: src/usr.sbin/ipsend/iptest/Attic/iptest.c,v 1.6 2001/01/17 06:11:15 fgsch Exp $	*/
 
 /*
  * ipsend.c (C) 1995-1998 Darren Reed
