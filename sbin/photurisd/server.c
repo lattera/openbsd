cd147b9aba41215a6ad2d921fee193c1e8ff69f9
deraadt <deraadt>
2001-01-17 19:41:08 +0000

    remove signal race, by checking signal flag in main loop; Niels looked at it

--- sbin/photurisd/server.c
+++ sbin/photurisd/server.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: server.c,v 1.6 2000/12/14 23:28:59 provos Exp $";
+static char rcsid[] = "$Id: server.c,v 1.7 2000/12/15 02:50:39 provos Exp $";
 #endif
 
 #define _SERVER_C_
@@ -273,6 +273,13 @@ server(void)
 	  FD_SET(sockets[i], normfds);
 
      while (1) {
+	  extern sig_atomic_t wantconfig;
+
+	  if (wantconfig) {
+		reconfig(0);
+		wantconfig = 0;
+	  }
+
 	  bcopy(normfds, readfds, size);
 
 	  /* Timeout till next job */
