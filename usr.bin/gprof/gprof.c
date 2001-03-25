459802b159ff18283124b8feb788e2a6d7c1cabb
mickey <mickey>
2001-03-25 19:23:40 +0000

    extern __progname; from bob

--- usr.bin/gprof/gprof.c
+++ usr.bin/gprof/gprof.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.bin/gprof/gprof.c,v 1.7 2001/03/22 05:18:30 mickey Exp $	*/
+/*	$OpenBSD: src/usr.bin/gprof/gprof.c,v 1.8 2001/03/25 19:23:40 mickey Exp $	*/
 /*	$NetBSD: gprof.c,v 1.8 1995/04/19 07:15:59 cgd Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)gprof.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: src/usr.bin/gprof/gprof.c,v 1.7 2001/03/22 05:18:30 mickey Exp $";
+static char rcsid[] = "$OpenBSD: src/usr.bin/gprof/gprof.c,v 1.8 2001/03/25 19:23:40 mickey Exp $";
 #endif
 #endif /* not lint */
 
@@ -56,6 +56,7 @@ static char rcsid[] = "$OpenBSD: src/usr.bin/gprof/gprof.c,v 1.7 2001/03/22 05:1
 char	*defaultEs[] = { "mcount" , "__mcleanup" , 0 };
 
 static struct gmonhdr	gmonhdr;
+extern char *__progname;
 
 int
 main(argc, argv)
