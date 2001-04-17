539779cbc526b1efb7acfe7ffb8d6c383280590c
aaron <aaron>
2001-04-17 04:30:51 +0000

    Implement cnbell(), an optional entrypoint that rings the console bell; from NetBSD. deraadt@ ok

--- sys/arch/mvme68k/mvme68k/machdep.c
+++ sys/arch/mvme68k/mvme68k/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/mvme68k/mvme68k/machdep.c,v 1.35 2001/04/05 20:39:39 deraadt Exp $ */
+/*	$OpenBSD: src/sys/arch/mvme68k/mvme68k/machdep.c,v 1.36 2001/04/17 04:30:46 aaron Exp $ */
 
 /*
  * Copyright (c) 1995 Theo de Raadt
@@ -173,6 +173,7 @@ static struct consdev bootcons = {
 	bootcngetc, 
 	bootcnputc,
 	bootcnpollc, 
+	NULL,
 	makedev(14,0), 
 	1};
 
