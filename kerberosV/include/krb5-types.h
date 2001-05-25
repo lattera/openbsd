48fd9a281b0f68e9c1a411f29144f201bc2311db
hin <hin>
2001-05-25 08:17:54 +0000

    Include stuff needed for Heimdal.

---
+++ kerberosV/include/krb5-types.h
@@ -0,0 +1,10 @@
+/* krb5-types.h -- this file was generated for i386-unknown-openbsd2.7 by
+                   $Id: krb5-types.h,v 1.2 2000/08/01 17:43:43 hin Exp $ */
+
+#ifndef __krb5_types_h__
+#define __krb5_types_h__
+
+#include <sys/types.h>
+#include <inttypes.h>
+
+#endif /* __krb5_types_h__ */
