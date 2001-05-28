5ff48e763e90f3aec0bcac9bc44178d261f789cd
drahn <drahn>
2001-05-28 21:38:14 +0000

    Commonize csu code for elf systems, powerpc now no longer has it's own versions of these files.
    Fixed a bug in ld.so in this, instead of scheduling the fini of each of
    the shared libraries with atexit. schedule a function of ld.so itself
    and it will walk all of the open libraries when the program exits.
    otherwise a shared library could be dl_open()ed and then dl_close()d
    and then it would not be mapped for the atexit processing.
    
    TODO:
    What if atexit is not found (process did not link against libc?)
    Do shared libraries that are dl_closed have their global destructors run?

--- lib/csu/powerpc/crtend.c
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD: src/lib/csu/powerpc/Attic/crtend.c,v 1.2 1999/08/20 14:11:36 niklas Exp $	*/
-/*	$NetBSD: crtend.c,v 1.1 1997/04/16 19:38:24 thorpej Exp $	*/
-
-#include <sys/cdefs.h>
-
-static void (*__CTOR_LIST__[1]) __P((void))
-    __attribute__((section(".ctors"))) = { (void *)0 };		/* XXX */
-static void (*__DTOR_LIST__[1]) __P((void))
-    __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
