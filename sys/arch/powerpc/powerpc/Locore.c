45639a9e204e292028b52dc6b503fb02a88bdb42
drahn <drahn>
2001-06-24 04:46:25 +0000

    include systm.h for proper prototypes.

--- sys/arch/powerpc/powerpc/Locore.c
+++ sys/arch/powerpc/powerpc/Locore.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/powerpc/powerpc/Attic/Locore.c,v 1.5 1997/10/13 13:42:52 pefo Exp $	*/
+/*	$OpenBSD: src/sys/arch/powerpc/powerpc/Attic/Locore.c,v 1.6 2001/06/24 04:46:25 drahn Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -38,6 +38,7 @@
 
 #include <sys/param.h>
 #include <sys/proc.h>
+#include <sys/systm.h>
 
 int whichqs;
 
