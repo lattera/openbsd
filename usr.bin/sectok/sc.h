f73683b297052f7579fd1ebd9fcd2fa080923800
rees <rees>
2001-07-17 21:04:14 +0000

    now you can display and set acls use sectok_apdu for "apdu" command

--- usr.bin/sectok/sc.h
+++ usr.bin/sectok/sc.h
@@ -1,4 +1,4 @@
-/* $Id: sc.h,v 1.4 2001/07/16 23:09:37 rees Exp $ */
+/* $Id: sc.h,v 1.5 2001/07/17 15:16:46 rees Exp $ */
 
 /*
  * Smartcard commander.
@@ -50,6 +50,7 @@ int class(int ac, char *av[]);
 int dread(int ac, char *av[]);
 int dwrite(int ac, char *av[]);
 int ls(int ac, char *av[]);
+int acl(int ac, char *av[]);
 int jcreate(int ac, char *av[]);
 int jdelete(int ac, char *av[]);
 int jdefault(int ac, char *av[]);
