4078ceaac31334ce860906a52626971ff5862531
millert <millert>
2001-08-30 17:38:13 +0000

    o kill register o add const to rcsid and copyright strings o placate -Wall

--- usr.sbin/lpr/common_source/startdaemon.c
+++ usr.sbin/lpr/common_source/startdaemon.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/lpr/common_source/startdaemon.c,v 1.3 1997/01/17 16:11:38 millert Exp $	*/
+/*	$OpenBSD: src/usr.sbin/lpr/common_source/startdaemon.c,v 1.4 2001/08/30 17:38:13 millert Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993, 1994
@@ -35,9 +35,9 @@
 
 #ifndef lint
 #if 0
-static char sccsid[] = "@(#)startdaemon.c	8.2 (Berkeley) 4/17/94";
+static const char sccsid[] = "@(#)startdaemon.c	8.2 (Berkeley) 4/17/94";
 #else
-static char rcsid[] = "$OpenBSD: src/usr.sbin/lpr/common_source/startdaemon.c,v 1.3 1997/01/17 16:11:38 millert Exp $";
+static const char rcsid[] = "$OpenBSD: src/usr.sbin/lpr/common_source/startdaemon.c,v 1.4 2001/08/30 17:38:13 millert Exp $";
 #endif
 #endif /* not lint */
 
@@ -67,7 +67,7 @@ startdaemon(printer)
 	char *printer;
 {
 	struct sockaddr_un un;
-	register int s, n;
+	int s, n;
 	char buf[BUFSIZ];
 
 	s = socket(AF_UNIX, SOCK_STREAM, 0);
