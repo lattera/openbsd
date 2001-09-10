ae3b992b6ec3d51f5021a357297e39c171960619
drahn <drahn>
2001-09-10 17:48:09 +0000

    mickey, at least compile them first. paddr_t should be protected with _LOCORE

--- sys/arch/macppc/include/pmap.h
+++ sys/arch/macppc/include/pmap.h
@@ -1,5 +1,7 @@
-/*	$OpenBSD: src/sys/arch/macppc/include/pmap.h,v 1.2 2001/09/10 16:44:52 mickey Exp $	*/
+/*	$OpenBSD: src/sys/arch/macppc/include/pmap.h,v 1.3 2001/09/10 17:48:09 drahn Exp $	*/
 
 #include <powerpc/pmap.h>
 
+#ifndef	_LOCORE
 paddr_t vtophys __P((vaddr_t));
+#endif	/* _LOCORE */
