38d67dba061b947ad8fc3e1e3018eb1115a30c1c
smurph <smurph>
2001-12-13 08:55:52 +0000

    Support for MVME197 completed.  Fix SPL defs.

--- sys/arch/mvme88k/dev/pcctwofunc.h
+++ sys/arch/mvme88k/dev/pcctwofunc.h
@@ -1,7 +1,7 @@
-/*	$OpenBSD: src/sys/arch/mvme88k/dev/Attic/pcctwofunc.h,v 1.1 2001/03/08 00:03:14 miod Exp $ */
+/*	$OpenBSD: src/sys/arch/mvme88k/dev/Attic/pcctwofunc.h,v 1.2 2001/12/13 08:55:51 smurph Exp $ */
 
-#ifndef _MVME88K_PCCTWO_H_
-#define _MVME88K_PCCTWO_H_
+#ifndef _MVME88K_PCCTWOF_H_
+#define _MVME88K_PCCTWOF_H_
 
 int pcctwointr_establish __P((int vec, struct intrhand *ih));
 
