4378d2e7a3df141db8aede4c57028a74f817fa18
art <art>
2002-02-18 11:27:45 +0000

    Clean up.

--- regress/lib/libc/longjmp/longjmp.c
+++ regress/lib/libc/longjmp/longjmp.c
@@ -1,6 +1,6 @@
-/*	$OpenBSD: src/regress/lib/libc/longjmp/longjmp.c,v 1.3 2002/01/04 13:48:52 art Exp $	*/
+/*	$OpenBSD: src/regress/lib/libc/longjmp/longjmp.c,v 1.4 2002/02/18 11:27:45 art Exp $	*/
 /*
- * Artur Grabowski <art@openbsd.org> Public Domain.
+ *	Artur Grabowski <art@openbsd.org>, 2002 Public Domain.
  */
 
 #include <stdio.h>
