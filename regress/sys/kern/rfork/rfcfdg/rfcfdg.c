d2071985d36301391f7ccabd2ad36384b810e8f0
deraadt <deraadt>
2002-02-21 23:29:05 +0000

    newlines at eof

--- regress/sys/kern/rfork/rfcfdg/rfcfdg.c
+++ regress/sys/kern/rfork/rfcfdg/rfcfdg.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/regress/sys/kern/rfork/rfcfdg/Attic/rfcfdg.c,v 1.1 2002/02/17 05:16:41 art Exp $	*/
+/*	$OpenBSD: src/regress/sys/kern/rfork/rfcfdg/Attic/rfcfdg.c,v 1.2 2002/02/21 23:29:05 deraadt Exp $	*/
 /*
  * Written by Artur Grabowski <art@openbsd.org>, 2002 Public Domain.
  */
