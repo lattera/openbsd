cc226a89ed19c1a5f70d3875de79a3f4d2e5330e
mickey <mickey>
2002-03-15 18:33:03 +0000

    a few missing rcsids

--- usr.bin/pmdb/arch/i386/pmdb_machdep.h
+++ usr.bin/pmdb/arch/i386/pmdb_machdep.h
@@ -1,3 +1,5 @@
+/*	$OpenBSD: src/usr.bin/pmdb/arch/i386/Attic/pmdb_machdep.h,v 1.2 2002/03/15 18:33:03 mickey Exp $	*/
+
 #define BREAKPOINT		{ 0xcc }
 #define BREAKPOINT_LEN		1
 #define BREAKPOINT_DECR_PC	1
