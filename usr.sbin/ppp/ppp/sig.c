3e0a721787e67f7b7c9ce439d66c011d68a3fedc
brian <brian>
2002-06-15 01:33:23 +0000

    Understand MS-MPPE-Encryption-Policy, MS-MPPE-Encryption-Types, MS-MPPE-Recv-Key and MS-MPPE-Send-Key RADIUS attributes, making MPPE work properly with RADIUS & MSCHAPv2.
    Do something with the Filter-Id RADIUS attribute.
    
    Handle MS-CHAP-Error and MS-CHAP2-Success correctly rather than
    bogusly including the Ident field in the text passed back to the
    client.
    
    Bump the max FSM option length to 52 (was 20)
    
    Add some missing includes.

--- usr.sbin/ppp/ppp/sig.c
+++ usr.sbin/ppp/ppp/sig.c
@@ -23,8 +23,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$OpenBSD: src/usr.sbin/ppp/ppp/sig.c,v 1.6 2002/05/16 01:13:39 brian Exp $
+ *	$OpenBSD: src/usr.sbin/ppp/ppp/sig.c,v 1.7 2002/06/15 01:33:23 brian Exp $
  */
+
+#include <sys/types.h>
 
 #include <signal.h>
 
