c781952476f5073b70617f2bf5cece4af47c82f7
millert <millert>
2002-07-09 18:59:12 +0000

    o Add _PATH_CRON_PID which is built up from PIDDIR and PIDFILE via string concatenation at compile time rather than run time.
    o Some minor cosmetic changes to simplify diffing.

--- usr.sbin/cron/env.c
+++ usr.sbin/cron/env.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/cron/env.c,v 1.11 2002/07/08 23:42:17 millert Exp $	*/
+/*	$OpenBSD: src/usr.sbin/cron/env.c,v 1.12 2002/07/09 18:59:12 millert Exp $	*/
 /* Copyright 1988,1990,1993,1994 by Paul Vixie
  * All rights reserved
  */
@@ -21,14 +21,14 @@
  */
 
 #if !defined(lint) && !defined(LINT)
-static char const rcsid[] = "$OpenBSD: src/usr.sbin/cron/env.c,v 1.11 2002/07/08 23:42:17 millert Exp $";
+static char const rcsid[] = "$OpenBSD: src/usr.sbin/cron/env.c,v 1.12 2002/07/09 18:59:12 millert Exp $";
 #endif
 
 #include "cron.h"
 
 char **
 env_init(void) {
-	char	**p = (char **) malloc(sizeof(char **));
+	char **p = (char **) malloc(sizeof(char **));
 
 	if (p != NULL)
 		p[0] = NULL;
@@ -37,9 +37,9 @@ env_init(void) {
 
 void
 env_free(char **envp) {
-	char	**p;
+	char **p;
 
-	for (p = envp;  *p != NULL;  p++)
+	for (p = envp; *p != NULL;  p++)
 		free(*p);
 	free(envp);
 }
