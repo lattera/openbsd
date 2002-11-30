6e48226292a83c7bf46946607b46fa81dab89947
deraadt <deraadt>
2002-11-30 00:20:46 +0000

    stop breaking the damn tree mickey

--- usr.sbin/tcpdump/interface.h
+++ usr.sbin/tcpdump/interface.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/tcpdump/interface.h,v 1.33 2002/11/29 18:27:54 mickey Exp $	*/
+/*	$OpenBSD: src/usr.sbin/tcpdump/interface.h,v 1.34 2002/11/30 00:20:46 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -194,8 +194,6 @@ extern void dvmrp_print(const u_char *, u_int);
 extern void egp_print(const u_char *, u_int, const u_char *);
 extern void enc_if_print(u_char *, const struct pcap_pkthdr *, const u_char *);
 extern void pflog_if_print(u_char *, const struct pcap_pkthdr *,
-        const u_char *);
-extern void pfsync_if_print(u_char *, const struct pcap_pkthdr *,
         const u_char *);
 extern void ether_if_print(u_char *, const struct pcap_pkthdr *,
 	const u_char *);
