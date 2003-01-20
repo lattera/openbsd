b389cb963cc9e62132258c9a03fc6a0957685a6e
marc <marc>
2003-01-20 18:14:07 +0000

    bye-bye libc_r sources. the sources have been moved (with history) to /usr/src/lib/libpthread

--- lib/libc_r/arch/alpha/_atomic_lock.c
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD: src/lib/libc_r/arch/alpha/Attic/_atomic_lock.c,v 1.7 2002/10/11 19:08:41 marc Exp $	*/
-/*
- * Atomi lock for alpha.
- */
-
-/* _atomic lock is implemented in assembler. */
--- lib/libc_r/arch/m88k/uthread_machdep.h
+++
@@ -1,4 +0,0 @@
-/*	$OpenBSD: src/lib/libc_r/arch/m88k/Attic/uthread_machdep.h,v 1.4 2001/02/21 00:24:38 miod Exp $	*/
-
-struct _machdep_state {
-};
