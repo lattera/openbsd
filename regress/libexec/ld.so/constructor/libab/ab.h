04383db8a3abcc2d1751d16fcda2f267ee085d0a
drahn <drahn>
2003-02-01 19:56:17 +0000

    Test programs for dependant shared library constructors.

---
+++ regress/libexec/ld.so/constructor/libab/ab.h
@@ -0,0 +1,11 @@
+/*
+ * Public Domain 2003 Dale Rahn
+ *
+ * $OpenBSD: src/regress/libexec/ld.so/constructor/libab/ab.h,v 1.1 2003/02/01 19:56:17 drahn Exp $
+ */
+
+class BB {
+public:
+        BB(char *);
+};
+
