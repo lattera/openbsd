fd1cc15277381b84ff5f8c285dd6cb1aa6b1d54f
cedric <cedric>
2003-02-11 12:21:32 +0000

    Show DF on IP fragments ok henric@

--- usr.sbin/tcpdump/print-ip.c
+++ usr.sbin/tcpdump/print-ip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/tcpdump/print-ip.c,v 1.17 2002/09/18 08:51:17 jakob Exp $	*/
+/*	$OpenBSD: src/usr.sbin/tcpdump/print-ip.c,v 1.18 2003/02/11 12:21:32 cedric Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -23,7 +23,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /cvs/src/usr.sbin/tcpdump/print-ip.c,v 1.16 2002/04/10 10:22:59 deraadt Exp $ (LBL)";
+    "@(#) $Header: /cvs/src/usr.sbin/tcpdump/print-ip.c,v 1.17 2002/09/18 08:51:17 jakob Exp $ (LBL)";
 #endif
 
 #include <sys/param.h>
@@ -573,7 +573,8 @@ ip_print(register const u_char *bp, register u_int length)
 		(void)printf(" (frag %d:%d@%d%s)", ntohs(ip->ip_id), len,
 			(off & 0x1fff) * 8,
 			(off & IP_MF)? "+" : "");
-	} else if (off & IP_DF)
+	} 
+	if (off & IP_DF)
 		(void)printf(" (DF)");
 
 	if (ip->ip_tos) {
