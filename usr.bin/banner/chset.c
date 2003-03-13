018dcc3b28f1adc8c43e6e6edb357eb7925139a9
tedu <tedu>
2003-03-13 23:11:16 +0000

    fix printing of "" and other weird chars.  from alexander@beard.se closes pr3146 and pr3147
    ok millert@

--- usr.bin/banner/chset.c
+++ usr.bin/banner/chset.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.bin/banner/chset.c,v 1.2 1996/06/26 05:31:35 deraadt Exp $	*/
+/*	$OpenBSD: src/usr.bin/banner/chset.c,v 1.3 2003/03/13 23:11:16 tedu Exp $	*/
 /*	$NetBSD: chset.c,v 1.2 1995/04/09 06:00:26 cgd Exp $	*/
 
 /*
@@ -56,7 +56,7 @@
 #if 0
 static char sccsid[] = "@(#)lpdchar.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$OpenBSD: src/usr.bin/banner/chset.c,v 1.2 1996/06/26 05:31:35 deraadt Exp $";
+static const char rcsid[] = "$OpenBSD: src/usr.bin/banner/chset.c,v 1.3 2003/03/13 23:11:16 tedu Exp $";
 #endif
 #endif /* not lint */
 
