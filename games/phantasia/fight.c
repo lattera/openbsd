97e4726e69f41b20e66f2b9be4048f9cbf1d58d5
deraadt <deraadt>
2003-04-25 21:37:47 +0000

    comment repair

--- games/phantasia/fight.c
+++ games/phantasia/fight.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/games/phantasia/fight.c,v 1.8 2003/04/06 18:50:38 deraadt Exp $	*/
+/*	$OpenBSD: src/games/phantasia/fight.c,v 1.9 2003/04/25 21:37:47 deraadt Exp $	*/
 /*	$NetBSD: fight.c,v 1.2 1995/03/24 03:58:39 cgd Exp $	*/
 
 /*
@@ -1017,7 +1017,7 @@ throwspell()
 / RETURN VALUE: none
 /
 / MODULES CALLED: truncstring(), fread(), fseek(), floor(), drandom(), 
-/	strcpy()
+/	strlcpy()
 /
 / GLOBAL INPUTS: Curmonster, Circle, Player, *Monstfp
 /
