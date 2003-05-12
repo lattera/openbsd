e4be8507ab0e5fb24aeefcdc6245af0446a071f0
camield <camield>
2003-05-12 19:28:22 +0000

    Sync to 0.6.2
    - UIDL digest calculation has been improved
    - minor bug fixes
    - -V switch to show version

---
+++ usr.sbin/popa3d/version.c
@@ -0,0 +1,9 @@
+/*	$OpenBSD: src/usr.sbin/popa3d/Attic/version.c,v 1.1 2003/05/12 19:28:22 camield Exp $	*/
+
+/*
+ * popa3d version information.
+ */
+
+char popa3d_version[] = "0.6.2";
+char popa3d_date[] = "$Date: 2003/03/10 00:30:38 $";
+char popa3d_id[] = "@(#) $Id: version.c,v 1.2 2003/03/10 00:30:38 solar Exp $";
