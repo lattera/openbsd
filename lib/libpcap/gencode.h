da81fd087d8736f20dcdac96176a427831fe1054
canacar <canacar>
2003-05-14 08:50:37 +0000

    libpcap and tcpdump now understand the new pflog datalink type. old datalink type is still recognized.
    ok henning@ dhartmei@ frantzen@

--- lib/libpcap/gencode.h
+++ lib/libpcap/gencode.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/lib/libpcap/gencode.h,v 1.10 2002/03/23 01:33:16 frantzen Exp $	*/
+/*	$OpenBSD: src/lib/libpcap/gencode.h,v 1.11 2003/05/14 08:50:37 canacar Exp $	*/
 
 /*
  * Copyright (c) 1990, 1991, 1992, 1993, 1994, 1995, 1996
@@ -20,7 +20,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: /cvs/src/lib/libpcap/gencode.h,v 1.9 2002/02/19 19:39:37 millert Exp $ (LBL)
+ * @(#) $Header: /cvs/src/lib/libpcap/gencode.h,v 1.10 2002/03/23 01:33:16 frantzen Exp $ (LBL)
  */
 
 /* Address qualifiers. */
@@ -178,6 +178,8 @@ struct block *gen_inbound(int);
 
 struct block *gen_pf_ifname(char *);
 struct block *gen_pf_rnr(int);
+struct block *gen_pf_srnr(int);
+struct block *gen_pf_ruleset(char *);
 struct block *gen_pf_reason(int);
 struct block *gen_pf_action(int);
 struct block *gen_pf_dir(int);
