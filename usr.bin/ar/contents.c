0373ed755cb1ae58c7b725b9ec3c997281df0da4
deraadt <deraadt>
2003-06-12 20:58:11 +0000

    de-register and ansification; millert ok

--- usr.bin/ar/contents.c
+++ usr.bin/ar/contents.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.bin/ar/Attic/contents.c,v 1.3 2003/06/03 02:56:05 millert Exp $	*/
+/*	$OpenBSD: src/usr.bin/ar/Attic/contents.c,v 1.4 2003/06/12 20:58:08 deraadt Exp $	*/
 /*	$NetBSD: contents.c,v 1.5 1995/03/26 03:27:49 glass Exp $	*/
 
 /*-
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)contents.c	8.3 (Berkeley) 4/2/94";
 #else
-static char rcsid[] = "$OpenBSD: src/usr.bin/ar/Attic/contents.c,v 1.3 2003/06/03 02:56:05 millert Exp $";
+static char rcsid[] = "$OpenBSD: src/usr.bin/ar/Attic/contents.c,v 1.4 2003/06/12 20:58:08 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -62,8 +62,7 @@ static char rcsid[] = "$OpenBSD: src/usr.bin/ar/Attic/contents.c,v 1.3 2003/06/0
  *	skipping member contents.
  */
 int
-contents(argv)
-	char **argv;
+contents(char **argv)
 {
 	int afd, all;
 	struct tm *tp;
