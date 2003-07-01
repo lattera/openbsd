52933a81778f6055d1e1824872fe171aa4812c5f
niklas <niklas>
2003-07-01 05:51:31 +0000

    A regression test for checking that dlopen prefers the correct shared library

---
+++ regress/libexec/ld.so/link-order/libnover/nover.c
@@ -0,0 +1,13 @@
+/*
+ * Public domain. 2003. Niklas Hallqvist.
+ *
+ * $OpenBSD: src/regress/libexec/ld.so/link-order/libnover/nover.c,v 1.1 2003/07/01 05:51:31 niklas Exp $
+ */
+
+#include <stdio.h>
+
+void
+version(void)
+{
+	printf("none\n");
+}
