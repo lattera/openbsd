94c0d17f6a98b0fe82fc070048473c967ad4d9d3
mickey <mickey>
2003-07-24 22:00:24 +0000

    kill some externs that are not there anymore and had left other places for a better void ages ago

--- sys/xfs/xfs_fs.h
+++ sys/xfs/xfs_fs.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  */
 
-/* $Id: xfs_fs.h,v 1.1.1.1 2002/06/07 03:32:59 hin Exp $ */
+/* $Id: xfs_fs.h,v 1.6 2002/06/07 04:10:32 hin Exp $ */
 
 #ifndef _xfs_h
 #define _xfs_h
@@ -71,8 +71,6 @@ struct xfs {
 #define XFS_FROM_XNODE(xp) XFS_FROM_VNODE(XNODE_TO_VNODE(xp))
 
 extern struct xfs xfs[];
-
-extern struct vnodeops xfs_vnodeops;
 
 struct xfs_node *xfs_node_find(struct xfs *, struct xfs_handle *);
 int new_xfs_node(struct xfs *, struct xfs_msg_node *, struct xfs_node **,
