34220274dbd750cc8cbe8d7c25ec45db1461f566
deraadt <deraadt>
2003-07-28 20:38:31 +0000

    cleanup library parts

--- usr.bin/lex/libmain.c
+++ usr.bin/lex/libmain.c
@@ -1,23 +1,20 @@
-/*	$OpenBSD: src/usr.bin/lex/libmain.c,v 1.5 2002/02/16 21:27:47 millert Exp $	*/
+/*	$OpenBSD: src/usr.bin/lex/libmain.c,v 1.6 2003/07/28 20:38:31 deraadt Exp $	*/
 
 /* libmain - flex run-time support library "main" function */
 
-/* $Header: /cvs/src/usr.bin/lex/libmain.c,v 1.4 1996/12/14 07:07:56 tholo Exp $ */
+/* $Header: /cvs/src/usr.bin/lex/libmain.c,v 1.5 2002/02/16 21:27:47 millert Exp $ */
 
 #include <sys/cdefs.h>
 
 int yylex(void);
-int main(int, char **, char **);
+int main(int, char **);
 
 /* ARGSUSED */
 int
-main( argc, argv, envp )
-int argc;
-char *argv[];
-char *envp[];
-	{
-	while ( yylex() != 0 )
+main(int argc, char *argv[])
+{
+	while (yylex() != 0)
 		;
 
 	return 0;
-	}
+}
--- usr.bin/lex/libyywrap.c
+++ usr.bin/lex/libyywrap.c
@@ -1,15 +1,15 @@
-/*	$OpenBSD: src/usr.bin/lex/libyywrap.c,v 1.5 2002/02/16 21:27:47 millert Exp $	*/
+/*	$OpenBSD: src/usr.bin/lex/libyywrap.c,v 1.6 2003/07/28 20:38:31 deraadt Exp $	*/
 
 /* libyywrap - flex run-time support library "yywrap" function */
 
-/* $Header: /cvs/src/usr.bin/lex/libyywrap.c,v 1.4 1996/12/10 22:22:03 millert Exp $ */
+/* $Header: /cvs/src/usr.bin/lex/libyywrap.c,v 1.5 2002/02/16 21:27:47 millert Exp $ */
 
 #include <sys/cdefs.h>
 
 int yywrap(void);
 
 int
-yywrap()
-	{
+yywrap(void)
+{
 	return 1;
-	}
+}
