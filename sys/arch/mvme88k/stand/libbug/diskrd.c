903f68653fd711ba9da57ba712bb146a549f7e39
miod <miod>
2003-09-07 21:35:35 +0000

    Oops, use the local MVMEPROM_CALL(), rather than the one which used to be in <machine/prom.h> -- code in stand/ does not need a specific context to invoke the BUG functions.

--- sys/arch/mvme88k/stand/libbug/diskrd.c
+++ sys/arch/mvme88k/stand/libbug/diskrd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/mvme88k/stand/libbug/diskrd.c,v 1.3 2001/07/04 08:09:26 niklas Exp $	*/
+/*	$OpenBSD: src/sys/arch/mvme88k/stand/libbug/diskrd.c,v 1.4 2003/09/07 21:35:35 miod Exp $	*/
 
 /*
  * bug routines -- assumes that the necessary sections of memory
@@ -6,6 +6,7 @@
  */
 #include <sys/types.h>
 #include <machine/prom.h>
+#include "prom.h"
 
 /* returns 0: success, nonzero: error */
 int
