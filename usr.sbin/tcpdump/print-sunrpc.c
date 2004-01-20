7a73d34c768e3e067a61956ed723ad58e02d797d
otto <otto>
2004-01-20 09:00:41 +0000

    If you go through the trouble of caching the name of the last printed rpc num, you might as well use it later too. ok canacar@

--- usr.sbin/tcpdump/print-sunrpc.c
+++ usr.sbin/tcpdump/print-sunrpc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/tcpdump/print-sunrpc.c,v 1.13 2002/02/19 19:39:40 millert Exp $	*/
+/*	$OpenBSD: src/usr.sbin/tcpdump/print-sunrpc.c,v 1.14 2004/01/20 09:00:41 otto Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993, 1994, 1995, 1996
@@ -23,7 +23,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /cvs/src/usr.sbin/tcpdump/print-sunrpc.c,v 1.12 2001/11/07 18:48:16 deraadt Exp $ (LBL)";
+    "@(#) $Header: /cvs/src/usr.sbin/tcpdump/print-sunrpc.c,v 1.13 2002/02/19 19:39:40 millert Exp $ (LBL)";
 #endif
 
 #include <sys/param.h>
@@ -111,6 +111,7 @@ progstr(prog)
 
 	if (lastprog != 0 && prog == lastprog)
 		return (buf);
+	lastprog = prog;
 	rp = getrpcbynumber(prog);
 	if (rp == NULL)
 		snprintf(buf, sizeof(buf), "#%u", prog);
