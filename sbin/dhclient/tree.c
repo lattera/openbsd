acd1de9d2f2d2d7505e99dcf26b548b7d0fec7cf
henning <henning>
2004-02-24 13:08:26 +0000

    cut more crap - no binary change

--- sbin/dhclient/tree.c
+++ sbin/dhclient/tree.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sbin/dhclient/Attic/tree.c,v 1.7 2004/02/24 00:34:40 henning Exp $	*/
+/*	$OpenBSD: src/sbin/dhclient/Attic/tree.c,v 1.8 2004/02/24 13:08:26 henning Exp $	*/
 
 /* Routines for manipulating parse trees... */
 
@@ -41,6 +41,8 @@
  */
 
 #include "dhcpd.h"
+
+extern int h_errno;
 
 static time_t tree_evaluate_recurse(
     int *, unsigned char **, int *, struct tree *);
