ad72920b7b41c4e7df44e256430ea4c23e233377
millert <millert>
2004-10-04 15:19:04 +0000

    Use poll(2) and svc_getreq_poll(2).  Tested by matthieu@, ok deraadt@

--- usr.sbin/amd/amd/sched.c
+++ usr.sbin/amd/amd/sched.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/amd/amd/sched.c,v 1.11 2004/03/15 08:54:54 djm Exp $	*/
+/*	$OpenBSD: src/usr.sbin/amd/amd/sched.c,v 1.12 2004/10/04 15:19:04 millert Exp $	*/
 
 /*
  * Copyright (c) 1990 Jan-Simon Pendry
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)sched.c	8.1 (Berkeley) 6/6/93
- *	$Id: sched.c,v 1.10 2003/10/30 16:04:06 millert Exp $
+ *	$Id: sched.c,v 1.11 2004/03/15 08:54:54 djm Exp $
  */
 
 /*
@@ -45,8 +45,8 @@
 #include <signal.h>
 #include WAIT
 #include <setjmp.h>
-extern jmp_buf select_intr;
-extern int select_intr_valid;
+extern jmp_buf poll_intr;
+extern int poll_intr_valid;
 
 typedef struct pjob pjob;
 struct pjob {
@@ -232,8 +232,8 @@ sigchld(int sig)
 #ifdef SYS5_SIGNALS
 	signal(sig, sigchld);
 #endif /* SYS5_SIGNALS */
-	if (select_intr_valid)
-		longjmp(select_intr, sig);
+	if (poll_intr_valid)
+		longjmp(poll_intr, sig);
 	errno = save_errno;
 }
 
