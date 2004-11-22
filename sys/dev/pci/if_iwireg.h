5fe3d135d9f040e8da15707bf78bf768231c1b3e
damien <damien>
2004-11-22 21:34:35 +0000

    use the filesystem based firmware loader; ok deraadt@

--- sys/dev/pci/if_iwireg.h
+++ sys/dev/pci/if_iwireg.h
@@ -1,4 +1,4 @@
-/*	$Id: if_iwireg.h,v 1.1 2004/10/20 12:50:48 deraadt Exp $ */
+/*	$Id: if_iwireg.h,v 1.2 2004/11/22 19:20:28 damien Exp $ */
 
 /*-
  * Copyright (c) 2004
@@ -106,6 +106,12 @@
 #define IWI_RATE_OFDM36	11
 #define IWI_RATE_OFDM48	1
 #define IWI_RATE_OFDM54	3
+
+/* firmware binary image header */
+struct iwi_firmware_hdr {
+	u_int32_t	version;
+	u_int32_t	mode;
+} __attribute__((__packed__));
 
 struct iwi_hdr {
 	u_int8_t	type;
