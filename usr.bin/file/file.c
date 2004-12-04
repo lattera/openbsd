ca7a579b8389e8b1d440df0edd94589ea4e49838
jaredy <jaredy>
2004-12-04 19:55:12 +0000

    sync manual and usage to file-4.09; bits from and ok ian and jmc

--- usr.bin/file/file.c
+++ usr.bin/file/file.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.bin/file/file.c,v 1.15 2004/09/25 09:19:35 otto Exp $ */
+/*	$OpenBSD: src/usr.bin/file/file.c,v 1.16 2004/12/04 19:55:12 jaredy Exp $ */
 /*
  * Copyright (c) Ian F. Darwin 1986-1995.
  * Software written by Ian F. Darwin and others;
@@ -73,7 +73,7 @@
 #include "patchlevel.h"
 
 #ifndef	lint
-FILE_RCSID("@(#)$Id: file.c,v 1.14 2004/05/19 02:32:35 tedu Exp $")
+FILE_RCSID("@(#)$Id: file.c,v 1.15 2004/09/25 09:19:35 otto Exp $")
 #endif	/* lint */
 
 
@@ -83,7 +83,8 @@ FILE_RCSID("@(#)$Id: file.c,v 1.14 2004/05/19 02:32:35 tedu Exp $")
 #define SYMLINKFLAG ""
 #endif
 
-# define USAGE  "Usage: %s [-bcik" SYMLINKFLAG "nNsvz] [-f namefile] [-F separator] [-m magicfiles] file...\n       %s -C -m magicfiles\n"
+#define USAGE	"Usage: %s [-bck" SYMLINKFLAG "Nnrsvz] [-F separator] [-f namefile] [-m magicfiles] file ...\n" \
+		"       %s [-m magicfiles] -C\n"
 
 #ifndef MAXPATHLEN
 #define	MAXPATHLEN	512
