bbecf36218c3fedb750be606e134de11eb3c9a4a
drahn <drahn>
2005-01-31 17:33:58 +0000

    fix botched tag.

--- sys/arch/zaurus/zaurus/zaurus_machdep.c
+++ sys/arch/zaurus/zaurus/zaurus_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD	*/
+/*	$OpenBSD: src/sys/arch/zaurus/zaurus/zaurus_machdep.c,v 1.9 2005/01/31 17:33:58 drahn Exp $	*/
 /*	$NetBSD: lubbock_machdep.c,v 1.2 2003/07/15 00:25:06 lukem Exp $ */
 
 /*
