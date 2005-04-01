b95bf50b693990cd27e4c9942858af1ec75b0f47
mickey <mickey>
2005-04-01 10:40:49 +0000

    small batch early bottling hppa64 port matured in mighty ukrainian oak for 23 months

---
+++ sys/arch/hppa64/include/iomod.h
@@ -0,0 +1,3 @@
+/*	$OpenBSD: src/sys/arch/hppa64/include/iomod.h,v 1.1 2005/04/01 10:40:48 mickey Exp $	*/
+
+#include <arch/hppa/include/iomod.h>
---
+++ sys/arch/hppa64/include/pdc.h
@@ -0,0 +1,3 @@
+/*	$OpenBSD: src/sys/arch/hppa64/include/pdc.h,v 1.1 2005/04/01 10:40:48 mickey Exp $	*/
+
+#include <arch/hppa/include/pdc.h>
