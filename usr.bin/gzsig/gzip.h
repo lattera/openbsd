1d029ada9c0ce9412684d1874fdf98743a7f8118
marius <marius>
2005-05-28 08:07:45 +0000

    whoops.  the import killed the $Id$ tags; rectify.

--- usr.bin/gzsig/gzip.h
+++ usr.bin/gzsig/gzip.h
@@ -28,7 +28,7 @@
  *   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: gzip.h,v 1.2 2005/04/01 16:47:31 dugsong Exp $
+ * $Vendor: gzip.h,v 1.2 2005/04/01 16:47:31 dugsong Exp $
  */
 
 #ifndef GZIP_H
--- usr.bin/gzsig/x509.h
+++ usr.bin/gzsig/x509.h
@@ -28,7 +28,7 @@
  *   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: x509.h,v 1.2 2005/04/01 16:47:31 dugsong Exp $
+ * $Vendor: x509.h,v 1.2 2005/04/01 16:47:31 dugsong Exp $
  */
 
 #ifndef X509_H
