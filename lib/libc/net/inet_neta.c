0c5dbc7c00afcb9d0f279a8cdb91bac00381a261
espie <espie>
2005-08-06 20:30:04 +0000

    zap rcsid. split functions off res_debug.c so that getnetnamadr.c does not pull it all when it's used.
    okay deraadt@, krw@

--- lib/libc/net/inet_neta.c
+++ lib/libc/net/inet_neta.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/lib/libc/net/inet_neta.c,v 1.6 2005/03/25 13:24:12 otto Exp $	*/
+/*	$OpenBSD: src/lib/libc/net/inet_neta.c,v 1.7 2005/08/06 20:30:03 espie Exp $	*/
 
 /*
  * Copyright (c) 1996 by Internet Software Consortium.
@@ -16,10 +16,6 @@
  * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
  * SOFTWARE.
  */
-
-#if defined(LIBC_SCCS) && !defined(lint)
-static const char rcsid[] = "$OpenBSD: src/lib/libc/net/inet_neta.c,v 1.6 2005/03/25 13:24:12 otto Exp $";
-#endif
 
 #include <sys/types.h>
 #include <sys/socket.h>
--- lib/libc/net/rcmdsh.c
+++ lib/libc/net/rcmdsh.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/lib/libc/net/rcmdsh.c,v 1.10 2005/03/25 13:24:12 otto Exp $	*/ 
+/*	$OpenBSD: src/lib/libc/net/rcmdsh.c,v 1.11 2005/08/06 20:30:03 espie Exp $	*/ 
 
 /*
  * Copyright (c) 2001, MagniComp
@@ -32,10 +32,6 @@
  * This is an rcmd() replacement originally by 
  * Chris Siebenmann <cks@utcc.utoronto.ca>.
  */
-
-#if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: src/lib/libc/net/rcmdsh.c,v 1.10 2005/03/25 13:24:12 otto Exp $";
-#endif /* LIBC_SCCS and not lint */
 
 #include      <sys/types.h>
 #include      <sys/socket.h>
