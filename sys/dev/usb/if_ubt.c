0a079a73e3529d02c5dca93d51e805f4718704b5
brad <brad>
2005-11-09 05:57:23 +0000

    splimp -> splnet

--- sys/dev/usb/if_ubt.c
+++ sys/dev/usb/if_ubt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/dev/usb/Attic/if_ubt.c,v 1.4 2005/08/02 12:32:07 reyk Exp $	*/
+/*	$OpenBSD: src/sys/dev/usb/Attic/if_ubt.c,v 1.5 2005/11/09 05:57:23 brad Exp $	*/
 
 /*
  * ng_ubt.c
@@ -27,7 +27,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: if_ubt.c,v 1.3 2005/08/01 05:36:48 brad Exp $
+ * $Id: if_ubt.c,v 1.4 2005/08/02 12:32:07 reyk Exp $
  * $FreeBSD: src/sys/netgraph/bluetooth/drivers/ubt/ng_ubt.c,v 1.20 2004/10/12 23:33:46 emax Exp $
  */
 
@@ -1713,7 +1713,7 @@ ubt_if_ioctl(struct ifnet *ifp, u_long command, caddr_t data)
 	int s;
 	int error = 0;
 
-	s = splimp();
+	s = splnet();
 	switch(command) {
 	case SIOCSIFADDR:
 		ifp->if_flags |= IFF_UP;
