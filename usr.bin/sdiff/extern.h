54e42180f30fa205a642774022bcdb989fa32c33
tedu <tedu>
2005-12-27 04:04:56 +0000

    add a public domain sdiff implementation contributed by ray lai

---
+++ usr.bin/sdiff/extern.h
@@ -0,0 +1,10 @@
+/*	$Id: extern.h,v 1.3 2005/12/15 16:19:27 ray Exp $	*/
+
+/*
+ * Written by Raymond Lai <ray@cyth.net>.
+ * Public domain.
+ */
+
+FILE	*outfile;	/* file to save changes to */
+
+int eparse(const char *, const char *, const char *);
