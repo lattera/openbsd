f738da9d47d748c4cfff3286f6ee6570486e8375
miod <miod>
2006-05-20 12:04:54 +0000

    Introduce a ``struct board'' platform abstraction, to be able to support more AViiON designs in the future. Heuristics to tell designs apart in aviion_identify() are likely too weak, but this is a start and AV400 still gets identified as AV400 (-:

--- sys/arch/aviion/include/trap.h
+++ sys/arch/aviion/include/trap.h
@@ -1,3 +1,4 @@
-/* $OpenBSD: src/sys/arch/aviion/include/trap.h,v 1.1.1.1 2006/04/18 10:56:57 miod Exp $ */
+/* $OpenBSD: src/sys/arch/aviion/include/trap.h,v 1.2 2006/05/20 12:04:54 miod Exp $ */
 /* public domain */
+#include <machine/board.h>	/* md_interrupt_func */
 #include <m88k/trap.h>
