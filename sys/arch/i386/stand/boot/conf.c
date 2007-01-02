51ac5a09c56398db74c3dd4f1521ca77c72feb08
tom <tom>
2007-01-02 16:29:27 +0000

    Fix the keyboard problem seen on Intel Macs, where only the first keypress is seen by boot.
    It appears that on the Intel Mac, we have to issue the "check for
    keystroke" BIOS call before the "get keystroke" call will get it
    (unlike any other BIOS I have seen in over 20 years).
    
    It would not have been possible to fix this problem without the
    donation from Steven N. Fettig (steve (at) anywheretechnology.com);
    many thanks.
    
    Bump versions of boot, cdboot and pxeboot accordingly.
    
    Testing kettenis@, otto@, and others; ok weingart@.

--- sys/arch/i386/stand/boot/conf.c
+++ sys/arch/i386/stand/boot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/i386/stand/boot/conf.c,v 1.34 2006/10/12 15:49:58 krw Exp $	*/
+/*	$OpenBSD: src/sys/arch/i386/stand/boot/conf.c,v 1.35 2007/01/02 16:29:27 tom Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -43,7 +43,7 @@
 #include <dev/cons.h>
 #include "debug.h"
 
-const char version[] = "2.12";
+const char version[] = "2.13";
 int	debug = 1;
 
 
