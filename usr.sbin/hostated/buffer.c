7d9ece52fdc34255169e86ea8b1a1120647a0079
reyk <reyk>
2007-01-08 13:37:26 +0000

    add a generic send/expect check using regular expression (see regex(3)).  this allows to define additional checks for other TCP protocols.
    From Pierre-Yves Ritschard (pyr at spootnik dot org)

--- usr.sbin/hostated/buffer.c
+++ usr.sbin/hostated/buffer.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/hostated/Attic/buffer.c,v 1.1 2006/12/16 11:45:07 reyk Exp $	*/
+/*	$OpenBSD: src/usr.sbin/hostated/Attic/buffer.c,v 1.2 2007/01/08 13:37:26 reyk Exp $	*/
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -29,6 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <regex.h>
 
 #include "hostated.h"
 
