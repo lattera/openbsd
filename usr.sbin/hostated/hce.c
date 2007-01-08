55655183f4f7cd7b1477077a473ce961cfd921db
reyk <reyk>
2007-01-08 20:46:18 +0000

    do NOT use the regexp interface. it is way to complicated, error-prone and we don't know about all the possible security problems.
    change the check send/expect code to use the fnmatch(3) interface
    using shell globbing rules instead. this allows simple patterns like
    "220 * ESMTP*" or "SSH-[12].??-*".
    
    suggested by deraadt@ and otto@
    ok Pierre-Yves Ritschard (pyr at spootnik dot org)

--- usr.sbin/hostated/hce.c
+++ usr.sbin/hostated/hce.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/hostated/Attic/hce.c,v 1.6 2007/01/08 16:52:58 reyk Exp $	*/
+/*	$OpenBSD: src/usr.sbin/hostated/Attic/hce.c,v 1.7 2007/01/08 20:46:18 reyk Exp $	*/
 
 /*
  * Copyright (c) 2006 Pierre-Yves Ritschard <pyr@spootnik.org>
@@ -35,7 +35,6 @@
 #include <unistd.h>
 #include <err.h>
 #include <pwd.h>
-#include <regex.h>
 
 #include "hostated.h"
 
