b3f9a61d0f2f5ceafc19ce53554f499e4370a435
deraadt <deraadt>
2007-06-20 18:18:47 +0000

    b_cylinder does not need to be set on the callpath down into drivers. cpu_disklabel can go away, since nothing anymore needs to use it; ok miod

--- sys/arch/arm/include/disklabel.h
+++ sys/arch/arm/include/disklabel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/arm/include/disklabel.h,v 1.12 2007/06/17 00:27:26 deraadt Exp $	*/
+/*	$OpenBSD: src/sys/arch/arm/include/disklabel.h,v 1.13 2007/06/20 18:15:45 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1994 Mark Brinicombe.
@@ -41,8 +41,5 @@
 #define LABELSECTOR	1		/* sector containing label */
 #define LABELOFFSET	0		/* offset of label in sector */
 #define MAXPARTITIONS	16		/* number of partitions */
-
-struct cpu_disklabel {
-};
 
 #endif /* _ARM_DISKLABEL_H_ */
