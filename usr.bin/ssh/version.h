44437fa8e45facf51f5b8eae085c4158fcc08e5c
markus <markus>
2007-08-15 08:16:49 +0000

    openssh 4.7

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.49 2007/03/06 10:13:14 djm Exp $ */
+/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.50 2007/08/15 08:16:49 markus Exp $ */
 
-#define SSH_VERSION	"OpenSSH_4.6"
+#define SSH_VERSION	"OpenSSH_4.7"
