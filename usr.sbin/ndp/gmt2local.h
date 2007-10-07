7209a2821310e569bdd6a48ddbaca0c23871e108
deraadt <deraadt>
2007-10-07 15:46:17 +0000

    delete ugly $Header stuff; pr 5595

--- usr.sbin/ndp/gmt2local.h
+++ usr.sbin/ndp/gmt2local.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/ndp/gmt2local.h,v 1.1 2004/01/25 20:45:13 deraadt Exp $	*/
+/*	$OpenBSD: src/usr.sbin/ndp/gmt2local.h,v 1.2 2007/10/07 15:46:17 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1997
@@ -20,7 +20,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: /cvs/src/usr.sbin/tcpdump/gmt2local.h,v 1.2 2000/10/03 14:31:55 ho Exp $ (LBL)
+ * @(#) $Id$ (LBL)
  */
 #ifndef gmt2local_h
 #define gmt2local_h
