9991b9fad0a74c79bd033e1ca946094f55b5be15
jasper <jasper>
2007-10-12 19:52:06 +0000

    use __progname where needed
    ok otto@

--- usr.bin/gzsig/sign.c
+++ usr.bin/gzsig/sign.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: src/usr.bin/gzsig/sign.c,v 1.8 2007/09/11 15:47:17 gilles Exp $ */
+/* $OpenBSD: src/usr.bin/gzsig/sign.c,v 1.9 2007/10/12 19:52:06 jasper Exp $ */
 
 /*
  * sign.c
@@ -185,7 +185,8 @@ embed_signature(struct key *key, FILE *fin, FILE *fout)
 void
 sign_usage(void)
 {
-	fprintf(stderr, "Usage: gzsig sign [-q] [-f secret_file] privkey [file ...]\n");
+	fprintf(stderr, "Usage: %s sign [-q] [-f secret_file] privkey [file ...]\n",
+	    __progname);
 }
 
 int
