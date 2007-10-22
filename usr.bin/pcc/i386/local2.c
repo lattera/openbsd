3aceda69d9ff73002f341daa5ca63317b214b550
otto <otto>
2007-10-22 13:40:08 +0000

    move from x86 to i386; gentle push by deraadt@, ok ragge@

--- usr.bin/pcc/i386/local2.c
+++ usr.bin/pcc/i386/local2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.bin/pcc/x86/Attic/local2.c,v 1.2 2007/10/14 19:58:00 stefan Exp $	*/
+/*	$OpenBSD: src/usr.bin/pcc/i386/Attic/local2.c,v 1.1 2007/10/22 13:39:37 otto Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
