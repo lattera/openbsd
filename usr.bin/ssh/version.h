61215f8e2b004449cb14ce605d99e05c2db395db
djm <djm>
2008-03-27 00:16:49 +0000

    openssh-4.9

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.51 2008/03/04 21:15:42 deraadt Exp $ */
+/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.52 2008/03/27 00:16:49 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_4.8"
+#define SSH_VERSION	"OpenSSH_4.9"
