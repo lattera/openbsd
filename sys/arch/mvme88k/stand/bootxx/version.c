f4bc2c6139cedf8e1d22dedcaeba09cc77af10bc
miod <miod>
2008-04-02 21:53:18 +0000

    Rewrite the startup code for the mvme88k bootblocks, once again. Some of them (especially the netboot ones) will now self-relocate, which makes BUG configuration much simpler.
    WARNING: for disk-based systems, you need to update bootxx and bootsd at the
    same time. New bootsd will not run when loaded from an older bootxx.
    This doesn't matter much since you need both with installboot.

--- sys/arch/mvme88k/stand/bootxx/version.c
+++ sys/arch/mvme88k/stand/bootxx/version.c
@@ -1,3 +1,7 @@
-/*	$OpenBSD: src/sys/arch/mvme88k/stand/bootxx/Attic/version.c,v 1.2 2006/05/16 22:52:55 miod Exp $ */
+/*	$OpenBSD: src/sys/arch/mvme88k/stand/bootxx/Attic/version.c,v 1.3 2008/04/02 21:53:17 miod Exp $ */
 
+/*
+ *	1.3	rewritten crt code, self-relocatable
+ *	1.2	rewritten startup code and general cleanup
+ */
 char *version = "1.2";
--- sys/arch/mvme88k/stand/netboot/version.c
+++ sys/arch/mvme88k/stand/netboot/version.c
@@ -1,7 +1,8 @@
-/*	$OpenBSD: src/sys/arch/mvme88k/stand/netboot/version.c,v 1.4 2008/03/31 22:14:43 miod Exp $ */
+/*	$OpenBSD: src/sys/arch/mvme88k/stand/netboot/version.c,v 1.5 2008/04/02 21:53:18 miod Exp $ */
 
 /*
+ *	1.5	rewritten crt code, self-relocatable
  *	1.4	kernel loaded with loadfile, a.out and ELF formats
  *	1.3	rewritten startup code and general cleanup
  */
-char *version = "1.4";
+char *version = "1.5";
