f689a2b3522caa1c2ad1bdbe1f99c38869d2cea8
miod <miod>
2008-05-21 19:50:31 +0000

    Remove <machine/elf_abi.h> on platforms which have it. Nothing in the tree needs the few MD definitions they (sometimes) provide; only binutils is interested in them, but binutils provide their own include files for that purpose anyway.
    ok deraadt@ kettenis@

--- sys/arch/mvmeppc/include/elf_abi.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: src/sys/arch/mvmeppc/include/Attic/elf_abi.h,v 1.2 2001/09/02 19:40:24 miod Exp $	*/
-
-#include <powerpc/elf_abi.h>
--- sys/arch/zaurus/include/elf_abi.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: src/sys/arch/zaurus/include/Attic/elf_abi.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
-
-#include <arm/elf_abi.h>
