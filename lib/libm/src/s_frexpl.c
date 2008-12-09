ef097552c04a1523b28da061dcafe08ea626efda
martynas <martynas>
2008-12-09 20:00:35 +0000

    - 80-bit and quad precision trigonometric and other most important functions: acosl, asinl, atanl, atan2l, cosl, sinl, tanl, exp2l, frexpl, ilogbl, ldexpl, logbl, scalbnl, fabsl, hypotl, powl, sqrtl, rintl, copysignl, nanl, fdiml, fmaxl, fminl.  mostly taken from freebsd, needed alot of changes to adapt.  note, these are all c versions;  and are quite slow when architectures have, e.g. sqrt.  assembly versions will be added afterwards - make them .weak/__weak_alias to the double precision versions on other archs - no need to have two finites.  finite() and finitef() are non-standard 3BSD obsolete versions of isfinite.  remove from libm.  make them weak_alias in libc to __isfinite and __isfinitef instead.  similarly make 3BSD obsolete versions of isinf, isinff, isnan, isnanf weak_aliases to C99's __isinf, __isinff, __isnan, __isnanf - remove unused infinity.c.  the c library has infinities for each supported platform - use STRICT_ASSIGN cast hack for _kernel_rem_pio2, so that the double version has a chance of working on i386 with extra precision - avoid storing multiple copies of the pi/2 array, since it won't vary - bump major due to removed finite/finitef.  although they will be in libc, which anything is linked to, minor bump might be enough ok millert@.   tested by sthen@, jsg@, ajacoutot@, kili@, naddy@

--- lib/libm/src/s_frexpl.c
+++ lib/libm/src/s_frexpl.c
@@ -1,11 +1,7 @@
-/*	$OpenBSD: src/lib/libm/arch/mc68881/Attic/s_finite.S,v 1.3 2005/08/02 11:17:32 espie Exp $ */
+/*	$OpenBSD: src/lib/libm/src/s_frexpl.c,v 1.1 2008/12/09 20:00:35 martynas Exp $	*/
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
+ * Copyright (c) 2004-2005 David Schultz <das@FreeBSD.ORG>
  * All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * the Systems Programming Group of the University of Utah Computer
- * Science Department.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -15,14 +11,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -30,20 +23,48 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * $FreeBSD: src/lib/msun/src/s_frexpl.c,v 1.1 2005/03/07 04:54:51 das Exp $
  */
 
-#include <machine/asm.h>
+#include <sys/types.h>
+#include <machine/ieee.h>
+#include <float.h>
+#include <math.h>
 
-| finite(x)
-| returns the value TRUE if -INF < x < +INF and returns FALSE otherwise.
-ENTRY(finite)
-	movw	#0x7FF0,d0
-	movw	sp@(4),d1
-	andw	d0,d1
-	cmpw	d0,d1
-	beq	Lnotfin
-	moveq	#1,d0
-	rts
-Lnotfin:
-	clrl	d0
-	rts
+#if LDBL_MAX_EXP != 0x4000
+#error "Unsupported long double format"
+#endif
+
+long double
+frexpl(long double x, int *ex)
+{
+	struct ieee_ext *p = (struct ieee_ext *)&x;
+
+	switch (p->ext_exp) {
+	case 0:		/* 0 or subnormal */
+		if ((p->ext_fracl
+#ifdef EXT_FRACLMBITS
+			| p->ext_fraclm
+#endif /* EXT_FRACLMBITS */
+#ifdef EXT_FRACHMBITS
+			| p->ext_frachm
+#endif /* EXT_FRACHMBITS */
+			| p->ext_frach) == 0) {
+			*ex = 0;
+		} else {
+			x *= 0x1.0p514;
+			*ex = p->ext_exp - 0x4200;
+			p->ext_exp = 0x3ffe;
+		}
+		break;
+	case 0x7fff:	/* infinity or NaN; value of *ex is unspecified */
+		break;
+	default:	/* normal */
+		*ex = p->ext_exp - 0x3ffe;
+		p->ext_exp = 0x3ffe;
+		break;
+	}
+
+	return x;
+}
