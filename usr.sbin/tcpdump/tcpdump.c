653990b79b113270d2687dbc7bf9182690969fb5
sthen <sthen>
2009-02-14 11:18:56 +0000

    increase the default snaplen to 116, allows capture of pflog+ipv6+tcp without knobs.  ok djm, deraadt.

--- usr.sbin/tcpdump/tcpdump.c
+++ usr.sbin/tcpdump/tcpdump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/tcpdump/tcpdump.c,v 1.60 2008/04/18 21:35:11 djm Exp $	*/
+/*	$OpenBSD: src/usr.sbin/tcpdump/tcpdump.c,v 1.61 2009/02/14 11:18:56 sthen Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -26,7 +26,7 @@ static const char copyright[] =
     "@(#) Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997\n\
 The Regents of the University of California.  All rights reserved.\n";
 static const char rcsid[] =
-    "@(#) $Id: tcpdump.c,v 1.59 2007/10/07 16:41:05 deraadt Exp $ (LBL)";
+    "@(#) $Id: tcpdump.c,v 1.60 2008/04/18 21:35:11 djm Exp $ (LBL)";
 #endif
 
 /*
@@ -352,7 +352,11 @@ main(int argc, char **argv)
 			else if (strcasecmp(optarg, "tcp") == 0)
 				packettype = PT_TCP;
 			else if (strcasecmp(optarg, "sack") == 0)
-				snaplen = SACK_SNAPLEN;
+				/*
+				 * kept for compatibility; DEFAULT_SNAPLEN
+				 * used to be too short to capture SACK.
+				 */
+				;
 			else
 				error("unknown packet type `%s'", optarg);
 			break;
