0873f8802bec8a13e1739221277e308f1d27f51d
miod <miod>
2009-02-17 18:42:45 +0000

    Forgot to crank version after ethernet buffer address change.

--- sys/arch/mvme68k/stand/netboot/version.c
+++ sys/arch/mvme68k/stand/netboot/version.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD: src/sys/arch/mvme68k/stand/netboot/version.c,v 1.6 1997/04/22 16:13:44 gvf Exp $ */
+/*	$OpenBSD: src/sys/arch/mvme68k/stand/netboot/version.c,v 1.7 2009/02/17 18:42:45 miod Exp $ */
 
 /*
  * make a random change to this file when you want the bootblock
  * revision to increase. like change this q to an x, or something.
  */
 
-char *version = "$Revision: 1.5 $";
+char *version = "$Revision: 1.7 $";
