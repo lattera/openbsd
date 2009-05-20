a6a3f2d8aee08823d2e6cd36b78a4b73aab95349
sthen <sthen>
2009-05-20 09:45:59 +0000

    "bgpctl ?" is invalid syntax; replace with "bgpctl help". from Aivar Jaakson.

--- usr.bin/bgplg/bgplg.h
+++ usr.bin/bgplg/bgplg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.bin/bgplg/bgplg.h,v 1.4 2007/04/13 15:31:02 claudio Exp $	*/
+/*	$OpenBSD: src/usr.bin/bgplg/bgplg.h,v 1.5 2009/05/20 09:45:59 sthen Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006 Reyk Floeter <reyk@vantronix.net>
@@ -64,7 +64,7 @@ struct cmd {
 	    { TRACEROUTE, "-Sl", NULL } },				\
 	{ "ping", 1, 1, "&lt;address&gt;",				\
 	    { PING, "-c4", "-w2", NULL } },				\
-	{ "?", 0, 0, NULL, { NULL }, lg_help },				\
+	{ "help", 0, 0, NULL, { NULL }, lg_help },			\
 	{ NULL }							\
 }
 
