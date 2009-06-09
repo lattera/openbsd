db622b9265898fbf24a1af931a0093db2939f40b
deraadt <deraadt>
2009-06-09 01:12:38 +0000

    backout interrupt diff until it the next round of fixes

--- sys/arch/macppc/include/intr.h
+++ sys/arch/macppc/include/intr.h
@@ -1,9 +1,9 @@
-/*	$OpenBSD: src/sys/arch/macppc/include/intr.h,v 1.7 2009/06/02 21:38:09 drahn Exp $	*/
+/*	$OpenBSD: src/sys/arch/macppc/include/intr.h,v 1.8 2009/06/09 01:12:38 deraadt Exp $	*/
 
 #include <powerpc/intr.h>
 
 #ifndef _LOCORE
 void softtty(void);
 
-void openpic_set_priority(int);
+void openpic_set_priority(int, int);
 #endif
