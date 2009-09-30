5bf8d2d8deac449ecdb54cc28ef5b5e8e1eede9d
syuu <syuu>
2009-09-30 06:22:00 +0000

    curproc, curprocpaddr moved into cpu_info OK miod@

--- sys/arch/sgi/include/asm.h
+++ sys/arch/sgi/include/asm.h
@@ -1,5 +1,14 @@
-/*	$OpenBSD: src/sys/arch/sgi/include/asm.h,v 1.1 2004/08/06 21:12:18 pefo Exp $ */
+/*	$OpenBSD: src/sys/arch/sgi/include/asm.h,v 1.2 2009/09/30 06:22:00 syuu Exp $ */
 
 /* Use Mips generic include file */
+
+#ifdef MULTIPROCESSOR
+#define HW_CPU_NUMBER(reg)			\
+	LA reg, HW_CPU_NUMBER_REG;		\
+	PTR_L reg, 0(reg)
+#else  /* MULTIPROCESSOR */
+#define HW_CPU_NUMBER(reg)			\
+	LI reg, 0
+#endif /* MULTIPROCESSOR */
 
 #include <mips64/asm.h>
