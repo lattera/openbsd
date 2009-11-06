3df9f21c54691bfb64f96131def4cd1e0779c452
miod <miod>
2009-11-06 07:37:49 +0000

    When rebooting IP27 or IP35 systems, do not request the prom to skip diagnostics anymore, as this causes the nsphy connected to the onboard iec to sometimes disappear after a  warm boot.
    Unfortunately this brings back the long reboot times on Origin 200, despite
    still asking for no memory test.

--- sys/arch/sgi/sgi/ip27_machdep.c
+++ sys/arch/sgi/sgi/ip27_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/sgi/sgi/ip27_machdep.c,v 1.31 2009/10/26 20:14:42 miod Exp $	*/
+/*	$OpenBSD: src/sys/arch/sgi/sgi/ip27_machdep.c,v 1.32 2009/11/06 07:37:49 miod Exp $	*/
 
 /*
  * Copyright (c) 2008, 2009 Miodrag Vallat.
@@ -539,7 +539,7 @@ ip27_halt(int howto)
 	} else
 		promop = GDA_PROMOP_REBOOT;
 
-	promop |= GDA_PROMOP_MAGIC | GDA_PROMOP_NO_DIAGS |
+	promop |= GDA_PROMOP_MAGIC | /* GDA_PROMOP_NO_DIAGS | */
 	    GDA_PROMOP_NO_MEMINIT;
 
 #if 0
