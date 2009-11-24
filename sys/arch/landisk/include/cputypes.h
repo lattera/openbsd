8780b3f1143bbf42daadeff24b8364fd122f8549
jasper <jasper>
2009-11-24 11:59:59 +0000

    - add rcs id's.

--- sys/arch/landisk/include/cputypes.h
+++ sys/arch/landisk/include/cputypes.h
@@ -1,1 +1,3 @@
+/*	$OpenBSD: src/sys/arch/landisk/include/cputypes.h,v 1.2 2009/11/24 11:59:59 jasper Exp $	*/
+
 #include <sh/cputypes.h>
--- sys/arch/landisk/include/lock.h
+++ sys/arch/landisk/include/lock.h
@@ -1,1 +1,3 @@
+/*	$OpenBSD: src/sys/arch/landisk/include/lock.h,v 1.2 2009/11/24 11:59:59 jasper Exp $	*/
+
 #include <sh/lock.h>
