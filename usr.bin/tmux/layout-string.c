071b3a20b5dae9c90a55a73d87b7ef3a76d536ef
nicm <nicm>
2010-01-08 17:44:27 +0000

    $OpenBSD$ not $Id$

--- usr.bin/tmux/layout-string.c
+++ usr.bin/tmux/layout-string.c
@@ -1,4 +1,4 @@
-/* $Id: layout-string.c,v 1.2 2010/01/07 20:28:01 nicm Exp $ */
+/* $OpenBSD: src/usr.bin/tmux/Attic/layout-string.c,v 1.4 2010/01/08 17:44:27 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
