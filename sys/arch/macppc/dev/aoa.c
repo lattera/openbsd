1170a0547e003b3bc1a18ca8f935005cd41a5102
jasper <jasper>
2010-02-26 21:52:14 +0000

    - this device doesn't provide volume control, so stop whining about it. it's just not there.
    ok jakemsr@ drahn@

--- sys/arch/macppc/dev/aoa.c
+++ sys/arch/macppc/dev/aoa.c
@@ -1,5 +1,4 @@
-/*	$OpenBSD: src/sys/arch/macppc/dev/aoa.c,v 1.5 2008/10/29 00:04:14 jakemsr Exp $	*/
-/*	$Id: aoa.c,v 1.4 2008/04/21 00:32:42 jakemsr Exp $	*/
+/*	$OpenBSD: src/sys/arch/macppc/dev/aoa.c,v 1.6 2010/02/26 21:52:14 jasper Exp $	*/
 
 /*-
  * Copyright (c) 2005 Tsubai Masanari.  All rights reserved.
@@ -149,7 +148,7 @@ aoa_getdev(void *h, struct audio_device *retp)
 void
 aoa_set_volume(struct aoa_softc *sc, int left, int right)
 {
-	printf("aoa_set_volume() not supported yet\n");
+	/* This device doesn't provide volume control. */
 }
 
 void
