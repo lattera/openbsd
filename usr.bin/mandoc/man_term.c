3d2f5252114b8592f237d22cdd6ab2ace88ae413
schwarze <schwarze>
2010-05-15 22:18:55 +0000

    For .IP, a single space after the head is sufficient; by Joerg Sonnenberger.

--- usr.bin/mandoc/man_term.c
+++ usr.bin/mandoc/man_term.c
@@ -1,4 +1,4 @@
-/*	$Id: man_term.c,v 1.31 2010/05/15 18:06:03 schwarze Exp $ */
+/*	$Id: man_term.c,v 1.32 2010/05/15 21:09:53 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
@@ -489,7 +489,6 @@ pre_IP(DECL_ARGS)
 		break;
 	case (MAN_HEAD):
 		p->flags |= TERMP_NOBREAK;
-		p->flags |= TERMP_TWOSPACE;
 		break;
 	case (MAN_BLOCK):
 		print_bvspace(p, n);
@@ -550,7 +549,6 @@ post_IP(DECL_ARGS)
 	case (MAN_HEAD):
 		term_flushln(p);
 		p->flags &= ~TERMP_NOBREAK;
-		p->flags &= ~TERMP_TWOSPACE;
 		p->rmargin = p->maxrmargin;
 		break;
 	case (MAN_BODY):
