3b4b207ff6503d172f37f691080842d5f4d996b3
jsing <jsing>
2010-07-02 23:24:21 +0000

    Pretend that softintr_establish() actually works.
    ok kettenis@

--- sys/arch/hppa64/hppa64/intr.c
+++ sys/arch/hppa64/hppa64/intr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/hppa64/hppa64/intr.c,v 1.1 2010/07/01 03:44:35 jsing Exp $	*/
+/*	$OpenBSD: src/sys/arch/hppa64/hppa64/intr.c,v 1.2 2010/07/02 23:24:21 jsing Exp $	*/
 
 /*
  * Copyright (c) Joel Sing <jsing@openbsd.org>
@@ -25,7 +25,7 @@ volatile u_long imask;
 void *
 softintr_establish(int pri, void (*handler)(void *), void *arg)
 {
-	return NULL;
+	return (void *)1;
 }
 
 void
