98ca358ed3419f72ad2cecbe243ba8acbdc5c415
miod <miod>
2010-11-20 20:15:58 +0000

    Remove unnecessary empty <machine/psl.h> files on arm-based platforms.

--- sys/arch/gumstix/include/psl.h
+++
@@ -1,1 +0,0 @@
-/*	$OpenBSD: src/sys/arch/gumstix/include/Attic/psl.h,v 1.1 2008/11/26 14:18:11 drahn Exp $	*/
--- sys/arch/zaurus/include/psl.h
+++
@@ -1,1 +0,0 @@
-/*	$OpenBSD: src/sys/arch/zaurus/include/Attic/psl.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
