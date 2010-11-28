908e0a361b6dbf8a76906a90c651c4f3ad82c45d
schwarze <schwarze>
2010-11-28 19:35:33 +0000

    To avoid FATAL errors, we have been parsing and ignoring the roff requests .am, .ami, .am1, .dei, and .rm for a long time. Since ignoring them can (rarely) cause information loss and serious misformatting, throw an ERROR: NOT IMPLEMENTED when finding them. Implementing them would not be too difficult, but they are so rare in practice that i can find better use for my time right now.
    In this context,
    - Put the string "NOT IMPLEMENTED" into two other error messages
    as well, to distinguish them from those caused by broken input.
    - Print the string "unknown macro" once, not twice in the error message
    associated with MANDOCERR_MACRO, and begin printing the buffer at the
    point where the unknown macro really is, not at the start of line.

--- usr.bin/mandoc/mdoc.c
+++ usr.bin/mandoc/mdoc.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc.c,v 1.67 2010/10/16 13:38:29 schwarze Exp $ */
+/*	$Id: mdoc.c,v 1.68 2010/10/16 20:49:37 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010 Ingo Schwarze <schwarze@openbsd.org>
@@ -783,9 +783,7 @@ mdoc_pmacro(struct mdoc *m, int ln, char *buf, int offs)
 
 	tok = (j > 1 || j < 4) ? mdoc_hash_find(mac) : MDOC_MAX;
 	if (MDOC_MAX == tok) {
-		mdoc_vmsg(m, MANDOCERR_MACRO, ln, sv, 
-		    "unknown macro: %s%s", 
-		    buf, strlen(buf) > 3 ? "..." : "");
+		mdoc_vmsg(m, MANDOCERR_MACRO, ln, sv, "%s", buf + sv - 1);
 		return(1);
 	}
 
