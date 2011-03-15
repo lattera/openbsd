73271cd81dab471b7da94a5d3f242f2ca3d91d7c
deraadt <deraadt>
2011-03-15 18:23:38 +0000

    unbreak tree for nested endian.h includes

--- sys/arch/mac68k/include/endian.h
+++ sys/arch/mac68k/include/endian.h
@@ -1,9 +1,4 @@
-/*	$OpenBSD: src/sys/arch/mac68k/include/Attic/endian.h,v 1.4 2011/03/11 15:17:08 pirofti Exp $	*/
+/*	$OpenBSD: src/sys/arch/mac68k/include/Attic/endian.h,v 1.5 2011/03/15 18:23:38 deraadt Exp $	*/
 /*	$NetBSD: endian.h,v 1.5 1994/10/26 08:46:28 cgd Exp $	*/
 
-#ifndef _MACHINE_ENDIAN_H_
-#define _MACHINE_ENDIAN_H_
-
 #include <m68k/endian.h>
-
-#endif /* _MACHINE_ENDIAN_H_ */
