687a524e4b0eb54d69d8e8ed02d3ac1db5cbef70
drahn <drahn>
2011-04-03 22:29:50 +0000

    Fix the really bad C++ test code that I wrote long ago.

--- regress/libexec/ld.so/dlopen/libac/ac.h
+++ regress/libexec/ld.so/dlopen/libac/ac.h
@@ -1,14 +1,14 @@
 /*
  * Public Domain 2003 Dale Rahn
  *
- * $OpenBSD: src/regress/libexec/ld.so/dlopen/libac/ac.h,v 1.1 2005/09/17 02:58:54 drahn Exp $
+ * $OpenBSD: src/regress/libexec/ld.so/dlopen/libac/ac.h,v 1.2 2011/04/03 22:29:50 drahn Exp $
  */
 
 class AC {
 public:
-        AC(char *);
+        AC(const char *);
         ~AC();
 private:
-	char *_name;
+	const char *_name;
 };
 
