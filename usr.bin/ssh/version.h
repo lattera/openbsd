37aa6c95c13fdc8d97124ea4d5ab92823bccfa53
djm <djm>
2011-08-02 23:13:01 +0000

    crank now, release later

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.61 2011/02/04 00:44:43 djm Exp $ */
+/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.62 2011/08/02 23:13:01 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_5.8"
+#define SSH_VERSION	"OpenSSH_5.9"
