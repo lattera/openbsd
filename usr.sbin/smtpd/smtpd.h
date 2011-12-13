ff88bc6c778515b22ba56c0e71164fe79367bb0f
gilles <gilles>
2011-12-13 23:55:00 +0000

    *finally* make use of certificate authority file if available !
    bits from relayd, ok chl@, ok eric@

--- usr.sbin/smtpd/smtpd.h
+++ usr.sbin/smtpd/smtpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/smtpd/smtpd.h,v 1.262 2011/12/13 22:04:35 eric Exp $	*/
+/*	$OpenBSD: src/usr.sbin/smtpd/smtpd.h,v 1.263 2011/12/13 23:55:00 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -477,6 +477,8 @@ enum session_state {
 struct ssl {
 	SPLAY_ENTRY(ssl)	 ssl_nodes;
 	char			 ssl_name[PATH_MAX];
+	char			*ssl_ca;
+	off_t			 ssl_ca_len;
 	char			*ssl_cert;
 	off_t			 ssl_cert_len;
 	char			*ssl_key;
