4704411854ba7a30ebb5c91c6faaea285b7dbaf6
schwarze <schwarze>
2012-02-26 19:41:27 +0000

    Support .OP, one of the extended man macros; from kristaps@. Do not use this GNU extension, we take it for compatibility only.

--- usr.bin/mandoc/man.c
+++ usr.bin/mandoc/man.c
@@ -1,4 +1,4 @@
-/*	$Id: man.c,v 1.63 2011/11/05 16:02:18 schwarze Exp $ */
+/*	$Id: man.c,v 1.64 2011/11/16 17:21:15 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -36,7 +36,7 @@ const	char *const __man_macronames[MAN_MAX] = {
 	"RI",		"na",		"sp",		"nf",
 	"fi",		"RE",		"RS",		"DT",
 	"UC",		"PD",		"AT",		"in",
-	"ft"
+	"ft",		"OP"
 	};
 
 const	char * const *man_macronames = __man_macronames;
