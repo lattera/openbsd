8ba273cbc970fff7d8a7f9476f35114478e78f8c
schwarze <schwarze>
2012-05-24 23:33:23 +0000

    Support -Ios='OpenBSD 5.1' to override uname(3) as the source of the default value for the mdoc(7) .Os macro. Needed for man.cgi on the OpenBSD website.
    Problem with man.cgi first noticed by deraadt@;
    beck@ and deraadt@ agree with the way to solve the issue.

--- usr.bin/mandoc/mandoc.h
+++ usr.bin/mandoc/mandoc.h
@@ -1,4 +1,4 @@
-/*	$Id: mandoc.h,v 1.42 2011/11/05 16:02:18 schwarze Exp $ */
+/*	$Id: mandoc.h,v 1.43 2011/11/12 22:43:18 schwarze Exp $ */
 /*
  * Copyright (c) 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -413,8 +413,8 @@ int		  mchars_spec2cp(const struct mchars *,
 			const char *, size_t);
 const char	 *mchars_spec2str(const struct mchars *, 
 			const char *, size_t, size_t *);
-struct mparse	 *mparse_alloc(enum mparset, 
-			enum mandoclevel, mandocmsg, void *);
+struct mparse	 *mparse_alloc(enum mparset, enum mandoclevel,
+			mandocmsg, void *, char *);
 void		  mparse_free(struct mparse *);
 void		  mparse_keep(struct mparse *);
 enum mandoclevel  mparse_readfd(struct mparse *, int, const char *);
