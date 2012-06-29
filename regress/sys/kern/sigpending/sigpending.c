29851bab4ca503e10c78d400852e912a9343a29c
matthew <matthew>
2012-06-29 00:34:39 +0000

    Fix year in public domain declaration.  Also, include <stddef.h> for POSIX compatibility so the test doesn't depend on <signal.h> defining NULL.

--- regress/sys/kern/sigpending/sigpending.c
+++ regress/sys/kern/sigpending/sigpending.c
@@ -1,11 +1,12 @@
-/*	$OpenBSD: src/regress/sys/kern/sigpending/sigpending.c,v 1.2 2012/06/29 00:29:17 matthew Exp $	*/
+/*	$OpenBSD: src/regress/sys/kern/sigpending/sigpending.c,v 1.3 2012/06/29 00:34:39 matthew Exp $	*/
 /*
- * Written by Matthew Dempsky, 2011.
+ * Written by Matthew Dempsky, 2012.
  * Public domain.
  */
 
 #include <assert.h>
 #include <signal.h>
+#include <stddef.h>
 
 int
 main()
