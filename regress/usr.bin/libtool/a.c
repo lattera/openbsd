43484f4e41b0fbb30710297dbdb976bbb4a85658
espie <espie>
2012-07-02 12:02:36 +0000

    basic libtool regression tests okay jasper@

---
+++ regress/usr.bin/libtool/a.c
@@ -0,0 +1,5 @@
+/* $OpenBSD: src/regress/usr.bin/libtool/a.c,v 1.1 2012/07/02 12:02:36 espie Exp $ */
+int f()
+{
+	return 42;
+}
---
+++ regress/usr.bin/libtool/c.c
@@ -0,0 +1,10 @@
+/* $OpenBSD: src/regress/usr.bin/libtool/c.c,v 1.1 2012/07/02 12:02:36 espie Exp $ */
+#include <stdio.h>
+
+extern int g();
+
+int main()
+{
+	printf("%d\n", g());
+	return 0;
+}
