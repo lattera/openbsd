00dcca3d9fc5fc2293835af168911df44f872979
miod <miod>
2012-09-29 18:54:39 +0000

    Proide a mips_sync() macro to wrap asm("sync"), and replace gazillions of such statements with it.

--- sys/arch/sgi/pci/macepcibridge.c
+++ sys/arch/sgi/pci/macepcibridge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/sgi/pci/macepcibridge.c,v 1.44 2012/03/15 18:52:56 miod Exp $ */
+/*	$OpenBSD: src/sys/arch/sgi/pci/macepcibridge.c,v 1.45 2012/09/29 18:54:39 miod Exp $ */
 
 /*
  * Copyright (c) 2009 Miodrag Vallat.
@@ -703,7 +703,7 @@ void
 mace_pcib_space_barrier(bus_space_tag_t t, bus_space_handle_t h,
     bus_size_t offs, bus_size_t len, int flags)
 {
-	__asm__ __volatile__ ("sync" ::: "memory");
+	mips_sync();
 }
 
 /*
