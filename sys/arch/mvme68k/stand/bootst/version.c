b5e29c262434a0512d9f0a14657e360c1cd1d6ea
miod <miod>
2012-12-31 21:35:32 +0000

    Switch mvme68k boot blocks to use the MI libsa loadfile routines to load kernel images, instead of their own; gives us the ability to load ELF kernels for free.
    Fix many warnings, and generally make the code closer to the current mvme88k
    boot blocks to help diffability.
    
    Rewrite bugcrt and bugexec() as a short assembly shim around a
    compiler-independent C block; this will allow compilers other than gcc2 to be
    used, and removes the need for bugcrt to be compiled without optimization.
    
    Crank all bootblocks versions.

--- sys/arch/mvme68k/stand/bootst/version.c
+++ sys/arch/mvme68k/stand/bootst/version.c
@@ -1,8 +1,7 @@
-/*	$OpenBSD: src/sys/arch/mvme68k/stand/bootst/version.c,v 1.3 1997/04/22 16:13:40 gvf Exp $ */
+/*	$OpenBSD: src/sys/arch/mvme68k/stand/bootst/version.c,v 1.4 2012/12/31 21:35:32 miod Exp $ */
 
 /*
- * make a random change to this file when you want the bootblock
- * revision to increase. like change this q to an x, or something.
+ *	1.4	kernel loaded with loadfile, a.out and ELF formats
  */
 
-char *version = "$Revision: 1.2 $";
+char *version = "1.4";
