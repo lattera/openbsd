3f07d701d320110751d927e4458ea65186501e42
gilles <gilles>
2013-01-26 09:37:24 +0000

    Sync with our smtpd repo:
    * first bricks of ldap and sqlite support (not finished but both working)
    * new table API to replace map API, all lookups are done through tables
    * improved handling of temporary errors throughout the daemon
    * improved scheduler and mta logic: connection reuse, optimizes batches
    * improved queue: more tolerant to admin errors, new layout, less disk-IO
    * improved memory usage under high load
    * SSL certs/keys isolated to lookup process to avoid facing network
    * VIRTUAL support improved, fully virtual setups possible now
    * runtime tracing of processes through smtpctl trace
    * ssl_privsep.c sync-ed with relayd
    * ssl.c no longer contains smtpd specific interfaces
    * smtpd-specific ssl bits moved to ssl_smtpd.c
    * update mail address in copyright
    
    FLUSH YOUR QUEUE. FLUSH YOUR QUEUE. FLUSH YOUR QUEUE. FLUSH YOUR QUEUE.
    
    smtpd.conf(5) simplified, it will require adaptations
    
    ok eric@

--- usr.sbin/smtpd/tree.c
+++ usr.sbin/smtpd/tree.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/usr.sbin/smtpd/tree.c,v 1.2 2012/11/20 09:47:46 eric Exp $	*/
+/*	$OpenBSD: src/usr.sbin/smtpd/tree.c,v 1.3 2013/01/26 09:37:24 gilles Exp $	*/
 
 /*
  * Copyright (c) 2012 Eric Faurot <eric@openbsd.org>
