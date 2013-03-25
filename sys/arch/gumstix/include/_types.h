8015eb524ecb01a8fcf708937871036c9f9ee4ea
deraadt <deraadt>
2013-03-25 17:33:02 +0000

    gumstix was never finished

--- sys/arch/gumstix/include/_types.h
+++
@@ -1,8 +0,0 @@
-/* 	$OpenBSD: src/sys/arch/gumstix/include/Attic/_types.h,v 1.5 2012/11/05 19:39:34 miod Exp $	*/
-
-#ifndef _MACHINE__TYPES_H_
-#define	_MACHINE__TYPES_H_
-
-#include <arm/_types.h>
-
-#endif
--- sys/arch/gumstix/include/disklabel.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: src/sys/arch/gumstix/include/Attic/disklabel.h,v 1.1 2008/11/26 14:18:11 drahn Exp $	*/
-
-#include <arm/disklabel.h>
