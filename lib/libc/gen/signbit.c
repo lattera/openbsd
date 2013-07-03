cfbf77cfa8a472ed782d00d3bae2a0967c31ff4b
espie <espie>
2013-07-03 04:46:36 +0000

    test LDBL_MANT_DIG == DBL_MANT_DIG instead of hardcoding 53. -> test becomes meaningful on vax No actual change, no bump okay martynas@

--- lib/libc/gen/signbit.c
+++ lib/libc/gen/signbit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/lib/libc/gen/signbit.c,v 1.6 2013/03/28 18:09:38 martynas Exp $	*/
+/*	$OpenBSD: src/lib/libc/gen/signbit.c,v 1.7 2013/07/03 04:46:36 espie Exp $	*/
 /*
  * Copyright (c) 2008 Martynas Venckus <martynas@openbsd.org>
  *
@@ -36,6 +36,6 @@ __signbitf(float f)
 	return p->sng_sign;
 }
 
-#if	LDBL_MANT_DIG == 53
+#if	LDBL_MANT_DIG == DBL_MANT_DIG
 __strong_alias(__signbitl, __signbit);
-#endif	/* LDBL_MANT_DIG == 53 */
+#endif	/* LDBL_MANT_DIG == DBL_MANT_DIG */
