d39419c1c44aa19b948ad3a3b0f336afd5f0c6af
miod <miod>
2013-10-16 16:59:35 +0000

    Support for oaic in the boot blocks. Cope with older prom putting a \r at the end of the commandline. Clean up a few stale comments. Crank boot blocks version to 0.3.

--- sys/arch/aviion/stand/boot/version.c
+++ sys/arch/aviion/stand/boot/version.c
@@ -1,3 +1,3 @@
-/*	$OpenBSD: src/sys/arch/aviion/stand/boot/version.c,v 1.2 2013/10/10 21:22:07 miod Exp $ */
+/*	$OpenBSD: src/sys/arch/aviion/stand/boot/version.c,v 1.3 2013/10/16 16:59:35 miod Exp $ */
 
-const char version[] = "0.2";
+const char version[] = "0.3";
