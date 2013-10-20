404862d8ed6a48bb28672550c640f32364212ebe
stsp <stsp>
2013-10-20 13:25:21 +0000

    Add i386/amd64 boot(8) support for keydisk-based softraid crypto volumes.
    So far, only passphrase-based crypto volumes were bootable. Full disk
    encryption with keydisks required a non-crypto partition to load the kernel.
    
    The bootloader now scans all BIOS-visible disks for RAID partitions and
    automatically associates keydisk partitions with their crypto volume.
    Attempting to boot from a volume without its keydisk currently results
    in a passphrase prompt (this might be changed in the future).
    
    There is no need to re-create existing volumes. Moving the root partition
    onto the crypto disk and running installboot(8) is all that's needed.
    
    help & ok jsing

--- sys/arch/i386/stand/boot/conf.c
+++ sys/arch/i386/stand/boot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/arch/i386/stand/boot/conf.c,v 1.50 2012/10/31 13:57:59 jsing Exp $	*/
+/*	$OpenBSD: src/sys/arch/i386/stand/boot/conf.c,v 1.51 2013/10/20 13:25:21 stsp Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -43,7 +43,7 @@
 #include <dev/cons.h>
 #include "debug.h"
 
-const char version[] = "3.21";
+const char version[] = "3.22";
 int	debug = 1;
 
 
