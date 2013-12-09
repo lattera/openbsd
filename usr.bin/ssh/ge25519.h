c7e01965b169939732eab9b13a40e3500721201a
markus <markus>
2013-12-09 11:03:45 +0000

    Add Authors for the public domain ed25519/nacl code. see also http://nacl.cr.yp.to/features.html 	All of the NaCl software is in the public domain. and http://ed25519.cr.yp.to/software.html 	The Ed25519 software is in the public domain.

--- usr.bin/ssh/ge25519.h
+++ usr.bin/ssh/ge25519.h
@@ -1,6 +1,10 @@
-/* $OpenBSD: src/usr.bin/ssh/ge25519.h,v 1.2 2013/12/07 00:26:37 djm Exp $ */
+/* $OpenBSD: src/usr.bin/ssh/ge25519.h,v 1.3 2013/12/09 11:03:45 markus Exp $ */
 
-/* Public Domain, from supercop-20130419/crypto_sign/ed25519/ref/ge25519.h */
+/*
+ * Public Domain, Authors: Daniel J. Bernstein, Niels Duif, Tanja Lange,
+ * Peter Schwabe, Bo-Yin Yang.
+ * Copied from supercop-20130419/crypto_sign/ed25519/ref/ge25519.h
+ */
 
 #ifndef GE25519_H
 #define GE25519_H
