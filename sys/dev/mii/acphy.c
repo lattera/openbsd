055ee3b9e1651daf0d317c68c65f3d62d19b69f5
deraadt <deraadt>
2013-12-28 03:30:41 +0000

    mii drivers no longer need activate functions.  Repair of the PHY configuration setting is done at resume time because all networks drivers which were previously up, do an IFF_UP operation which hits PHY_RESET. This was in snapshots for about 2 weeks.

--- sys/dev/mii/acphy.c
+++ sys/dev/mii/acphy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sys/dev/mii/acphy.c,v 1.6 2006/12/27 19:11:08 kettenis Exp $	*/
+/*	$OpenBSD: src/sys/dev/mii/acphy.c,v 1.7 2013/12/28 03:30:40 deraadt Exp $	*/
 /*	$NetBSD: acphy.c,v 1.13 2003/04/29 01:49:33 thorpej Exp $	*/
 
 /*
@@ -60,8 +60,7 @@ int	acphymatch(struct device *, void *, void *);
 void	acphyattach(struct device *, struct device *, void *);
 
 struct cfattach acphy_ca = {
-	sizeof(struct mii_softc), acphymatch, acphyattach, mii_phy_detach,
-	    mii_phy_activate
+	sizeof(struct mii_softc), acphymatch, acphyattach, mii_phy_detach
 };
 
 struct cfdriver acphy_cd = {
