7d0bafeb56bc0530f3582993bdfd69eb0989d7a5
schwarze <schwarze>
2014-01-06 00:53:14 +0000

    Joerg Sonnenberger contributed copyrightable amounts of text to some files.  To make it clear that he also put his contributions under the ISC license, with his explicit permission, add his Copyright notice to the relevant files.  No code change.

--- usr.bin/mandoc/read.c
+++ usr.bin/mandoc/read.c
@@ -1,7 +1,8 @@
-/*	$Id: read.c,v 1.18 2014/01/02 16:29:46 schwarze Exp $ */
+/*	$Id: read.c,v 1.19 2014/01/05 20:26:27 schwarze Exp $ */
 /*
  * Copyright (c) 2008, 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
+ * Copyright (c) 2010, 2012 Joerg Sonnenberger <joerg@netbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
