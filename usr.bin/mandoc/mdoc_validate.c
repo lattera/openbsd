c1ebc16afb6247c2568dff3d64d51b056272357d
schwarze <schwarze>
2014-01-11 22:16:03 +0000

    Remove useless use of strnlen(3). Yuckiness pointed out by deraadt@.

--- usr.bin/mandoc/mdoc_validate.c
+++ usr.bin/mandoc/mdoc_validate.c
@@ -1,4 +1,4 @@
-/*	$Id: mdoc_validate.c,v 1.118 2014/01/06 22:39:19 schwarze Exp $ */
+/*	$Id: mdoc_validate.c,v 1.119 2014/01/07 09:10:58 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2010-2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -1890,7 +1890,7 @@ post_hyph(POST_ARGS)
 		if (MDOC_TEXT != nch->type)
 			continue;
 		cp = nch->string;
-		if (3 > strnlen(cp, 3))
+		if ('\0' == *cp)
 			continue;
 		while ('\0' != *(++cp))
 			if ('-' == *cp &&
