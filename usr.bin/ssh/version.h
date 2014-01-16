c84c98ca04d247f3509868377a5b2d30aadad35e
djm <djm>
2014-01-16 07:32:00 +0000

    openssh-6.5

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.68 2013/11/08 01:38:11 djm Exp $ */
+/* $OpenBSD: src/usr.bin/ssh/version.h,v 1.69 2014/01/16 07:32:00 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_6.4"
+#define SSH_VERSION	"OpenSSH_6.5"
