eaaba37a045749c9de1a288f620cf2265144c7f1
markus <markus>
2014-01-28 20:13:46 +0000

    rename digest.c to digest-openssl.c and add libc variant; ok djm@

--- usr.bin/ssh/digest-libc.c
+++ usr.bin/ssh/digest-libc.c
@@ -1,6 +1,7 @@
-/* $OpenBSD: src/usr.bin/ssh/digest.c,v 1.4 2014/01/27 18:58:14 markus Exp $ */
+/* $OpenBSD: src/usr.bin/ssh/digest-libc.c,v 1.1 2014/01/28 20:13:46 markus Exp $ */
 /*
  * Copyright (c) 2013 Damien Miller <djm@mindrot.org>
+ * Copyright (c) 2014 Markus Friedl.  All rights reserved.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -20,32 +21,96 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <openssl/evp.h>
+#include <md5.h>
+#include <rmd160.h>
+#include <sha1.h>
+#include <sha2.h>
 
 #include "buffer.h"
 #include "digest.h"
 
+typedef void md_init_fn(void *mdctx);
+typedef void md_update_fn(void *mdctx, const u_int8_t *m, size_t mlen);
+typedef void md_final_fn(u_int8_t[], void *mdctx);
+
 struct ssh_digest_ctx {
 	int alg;
-	EVP_MD_CTX mdctx;
+	void *mdctx;
 };
 
 struct ssh_digest {
 	int id;
 	const char *name;
+	size_t block_len;
 	size_t digest_len;
-	const EVP_MD *(*mdfunc)(void);
+	size_t ctx_len;
+	md_init_fn *md_init;
+	md_update_fn *md_update;
+	md_final_fn *md_final;
 };
 
 /* NB. Indexed directly by algorithm number */
-const struct ssh_digest digests[] = {
-	{ SSH_DIGEST_MD5,	"MD5",	 	16,	EVP_md5 },
-	{ SSH_DIGEST_RIPEMD160,	"RIPEMD160",	20,	EVP_ripemd160 },
-	{ SSH_DIGEST_SHA1,	"SHA1",	 	20,	EVP_sha1 },
-	{ SSH_DIGEST_SHA256,	"SHA256", 	32,	EVP_sha256 },
-	{ SSH_DIGEST_SHA384,	"SHA384",	48,	EVP_sha384 },
-	{ SSH_DIGEST_SHA512,	"SHA512", 	64,	EVP_sha512 },
-	{ -1,			NULL,		0,	NULL },
+const struct ssh_digest digests[SSH_DIGEST_MAX] = {
+	{
+		SSH_DIGEST_MD5,
+		"MD5",
+		MD5_BLOCK_LENGTH,
+		MD5_DIGEST_LENGTH,
+		sizeof(MD5_CTX),
+		(md_init_fn *) MD5Init,
+		(md_update_fn *) MD5Update,
+		(md_final_fn *) MD5Final
+	},
+	{
+		SSH_DIGEST_RIPEMD160,
+		"RIPEMD160",
+		RMD160_BLOCK_LENGTH,
+		RMD160_DIGEST_LENGTH,
+		sizeof(RMD160_CTX),
+		(md_init_fn *) RMD160Init,
+		(md_update_fn *) RMD160Update,
+		(md_final_fn *) RMD160Final
+	},
+	{
+		SSH_DIGEST_SHA1,
+		"SHA1",
+		SHA1_BLOCK_LENGTH,
+		SHA1_DIGEST_LENGTH,
+		sizeof(SHA1_CTX),
+		(md_init_fn *) SHA1Init,
+		(md_update_fn *) SHA1Update,
+		(md_final_fn *) SHA1Final
+	},
+	{
+		SSH_DIGEST_SHA256,
+		"SHA256",
+		SHA256_BLOCK_LENGTH,
+		SHA256_DIGEST_LENGTH,
+		sizeof(SHA2_CTX),
+		(md_init_fn *) SHA256Init,
+		(md_update_fn *) SHA256Update,
+		(md_final_fn *) SHA256Final
+	},
+	{
+		SSH_DIGEST_SHA384,
+		"SHA384",
+		SHA384_BLOCK_LENGTH,
+		SHA384_DIGEST_LENGTH,
+		sizeof(SHA2_CTX),
+		(md_init_fn *) SHA384Init,
+		(md_update_fn *) SHA384Update,
+		(md_final_fn *) SHA384Final
+	},
+	{
+		SSH_DIGEST_SHA512,
+		"SHA512",
+		SHA512_BLOCK_LENGTH,
+		SHA512_DIGEST_LENGTH,
+		sizeof(SHA2_CTX),
+		(md_init_fn *) SHA512Init,
+		(md_update_fn *) SHA512Update,
+		(md_final_fn *) SHA512Final
+	}
 };
 
 static const struct ssh_digest *
@@ -69,7 +134,9 @@ ssh_digest_bytes(int alg)
 size_t
 ssh_digest_blocksize(struct ssh_digest_ctx *ctx)
 {
-	return EVP_MD_CTX_block_size(&ctx->mdctx);
+	const struct ssh_digest *digest = ssh_digest_by_alg(ctx->alg);
+
+	return digest == NULL ? 0 : digest->block_len;
 }
 
 struct ssh_digest_ctx *
@@ -78,31 +145,36 @@ ssh_digest_start(int alg)
 	const struct ssh_digest *digest = ssh_digest_by_alg(alg);
 	struct ssh_digest_ctx *ret;
 
-	if (digest == NULL || ((ret = calloc(1, sizeof(*ret))) == NULL))
+	if (digest == NULL || (ret = calloc(1, sizeof(ret))) == NULL)
 		return NULL;
-	ret->alg = alg;
-	EVP_MD_CTX_init(&ret->mdctx);
-	if (EVP_DigestInit_ex(&ret->mdctx, digest->mdfunc(), NULL) != 1) {
+	if ((ret->mdctx = calloc(1, digest->ctx_len)) == NULL) {
 		free(ret);
 		return NULL;
 	}
+	ret->alg = alg;
+	digest->md_init(ret->mdctx);
 	return ret;
 }
 
 int
 ssh_digest_copy_state(struct ssh_digest_ctx *from, struct ssh_digest_ctx *to)
 {
-	/* we have bcopy-style order while openssl has memcpy-style */
-	if (!EVP_MD_CTX_copy_ex(&to->mdctx, &from->mdctx))
+	const struct ssh_digest *digest = ssh_digest_by_alg(from->alg);
+
+	if (digest == NULL || from->alg != to->alg)
 		return -1;
+	memcpy(to->mdctx, from->mdctx, digest->ctx_len);
 	return 0;
 }
 
 int
 ssh_digest_update(struct ssh_digest_ctx *ctx, const void *m, size_t mlen)
 {
-	if (EVP_DigestUpdate(&ctx->mdctx, m, mlen) != 1)
+	const struct ssh_digest *digest = ssh_digest_by_alg(ctx->alg);
+
+	if (digest == NULL)
 		return -1;
+	digest->md_update(ctx->mdctx, m, mlen);
 	return 0;
 }
 
@@ -116,26 +188,30 @@ int
 ssh_digest_final(struct ssh_digest_ctx *ctx, u_char *d, size_t dlen)
 {
 	const struct ssh_digest *digest = ssh_digest_by_alg(ctx->alg);
-	u_int l = dlen;
 
+	if (digest == NULL)
+		return -1;
 	if (dlen > UINT_MAX)
 		return -1;
 	if (dlen < digest->digest_len) /* No truncation allowed */
 		return -1;
-	if (EVP_DigestFinal_ex(&ctx->mdctx, d, &l) != 1)
-		return -1;
-	if (l != digest->digest_len) /* sanity */
-		return -1;
+	digest->md_final(d, ctx->mdctx);
 	return 0;
 }
 
 void
 ssh_digest_free(struct ssh_digest_ctx *ctx)
 {
+	const struct ssh_digest *digest;
+
 	if (ctx != NULL) {
-		EVP_MD_CTX_cleanup(&ctx->mdctx);
-		memset(ctx, 0, sizeof(*ctx));
-		free(ctx);
+		digest = ssh_digest_by_alg(ctx->alg);
+		if (digest) {
+			memset(ctx->mdctx, 0, digest->ctx_len);
+			free(ctx->mdctx);
+			memset(ctx, 0, sizeof(*ctx));
+			free(ctx);
+		}
 	}
 }
 
