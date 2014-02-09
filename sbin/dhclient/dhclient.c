66389879b06bb9e2a148e01b98c41f5ddaccddc2
krw <krw>
2014-02-09 20:45:56 +0000

    Don't use imsg_flush(), roll a local flush_unpriv_ibuf() that loops on EAGAIN, sets quit to INTERNALSIG on errors (unless quit is already set to something else), and prints a consistant error message when errors other than EPIPE and end of file are encountered.
    Fixes failure to write resolv.conf when -L is used, and makes
    add_address() and add_route() also wait until imsg is in pipe.

--- sbin/dhclient/dhclient.c
+++ sbin/dhclient/dhclient.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: src/sbin/dhclient/dhclient.c,v 1.291 2014/02/07 18:42:17 krw Exp $	*/
+/*	$OpenBSD: src/sbin/dhclient/dhclient.c,v 1.293 2014/02/09 20:45:56 krw Exp $	*/
 
 /*
  * Copyright 2004 Henning Brauer <henning@openbsd.org>
@@ -563,7 +563,7 @@ main(int argc, char *argv[])
 	imsg_init(unpriv_ibuf, socket_fd[1]);
 
 	if ((fd = open(path_dhclient_db,
-	    O_RDONLY|O_EXLOCK|O_CREAT|O_NOFOLLOW, 0)) == -1)
+	    O_RDONLY|O_EXLOCK|O_CREAT|O_NOFOLLOW, 0640)) == -1)
 		error("can't open and lock %s: %s", path_dhclient_db,
 		    strerror(errno));
 	read_client_leases();
@@ -1733,7 +1733,7 @@ rewrite_option_db(struct client_lease *offered, struct client_lease *effective)
 
 	write_file(path_option_db,
 	    O_WRONLY | O_CREAT | O_TRUNC | O_SYNC | O_EXLOCK | O_NOFOLLOW,
-	    0, 0, 0, db, strlen(db));
+	    S_IRUSR | S_IWUSR | S_IRGRP, 0, 0, db, strlen(db));
 }
 
 char *
@@ -2419,10 +2419,7 @@ write_file(char *path, int flags, mode_t mode, uid_t uid, gid_t gid,
 	if (rslt == -1)
 		warning("write_file: imsg_composev: %s", strerror(errno));
 
-	/* Do flush to maximize chances of keeping file current. */
-	rslt = imsg_flush(unpriv_ibuf);
-	if (rslt == -1)
-		warning("write_file: imsg_flush: %s", strerror(errno));
+	flush_unpriv_ibuf("write_file");
 }
 
 void
