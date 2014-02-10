69b0abd3d96448c4fd6029a642f7ef0483d20aa2
jmatthew <jmatthew>
2014-02-10 22:41:27 +0000

    Build isp2xxx firmware into separate object files so we only include one copy when both isp(4) and qla(4) are enabled.  This is a temporary measure until qla(4) takes over completely.
    looked at by miod@ and kettenis@

---
+++ sys/dev/microcode/isp/isp_fw2100.c
@@ -0,0 +1,8 @@
+/* $OpenBSD: src/sys/dev/microcode/isp/isp_fw2100.c,v 1.1 2014/02/10 22:41:27 jmatthew Exp $ */
+/* public domain */
+
+#include <sys/types.h>
+
+#ifndef ISP_NOFIRMWARE
+#include <dev/microcode/isp/asm_2100.h>
+#endif
---
+++ sys/dev/microcode/isp/isp_fw2300.c
@@ -0,0 +1,8 @@
+/* $OpenBSD: src/sys/dev/microcode/isp/isp_fw2300.c,v 1.1 2014/02/10 22:41:27 jmatthew Exp $ */
+/* public domain */
+
+#include <sys/types.h>
+
+#ifndef ISP_NOFIRMWARE
+#include <dev/microcode/isp/asm_2300.h>
+#endif
