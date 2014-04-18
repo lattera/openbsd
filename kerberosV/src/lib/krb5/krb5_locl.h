109a48fdb66d612672750bcf83f703c5323399ca
ajacoutot <ajacoutot>
2014-04-18 09:47:36 +0000

    Upgrade our KerberosV to Heimdal 1.5.3 (minor update).
    - Fix leaking file descriptors in KDC
    - Better socket/timeout handling in libkrb5
    - General bug fixes
    
    ok robert@

--- kerberosV/src/lib/krb5/krb5_locl.h
+++ kerberosV/src/lib/krb5/krb5_locl.h
@@ -33,7 +33,7 @@
  * SUCH DAMAGE.
  */
 
-/* $Id$ */
+/* $Id: krb5_locl.h,v 1.9 2013/06/17 18:57:44 robert Exp $ */
 
 #ifndef __KRB5_LOCL_H__
 #define __KRB5_LOCL_H__
@@ -46,6 +46,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
+#ifdef HAVE_POLL_H
+#include <sys/poll.h>
+#endif
 
 #include <krb5-types.h>
 
