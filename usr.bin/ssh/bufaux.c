cad6a7bad23818cee6e48ee479b4697765b34239
markus <markus>
2014-04-29 18:01:49 +0000

    make compiling against OpenSSL optional (make OPENSSL=no); reduces algorithms to curve25519, aes-ctr, chacha, ed25519; allows us to explore further options; with and ok djm

--- usr.bin/ssh/bufaux.c
+++ usr.bin/ssh/bufaux.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: src/usr.bin/ssh/bufaux.c,v 1.58 2014/04/28 03:09:18 djm Exp $ */
+/* $OpenBSD: src/usr.bin/ssh/bufaux.c,v 1.59 2014/04/29 18:01:49 markus Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -38,8 +38,6 @@
  */
 
 #include <sys/types.h>
-
-#include <openssl/bn.h>
 
 #include <string.h>
 #include <stdarg.h>
