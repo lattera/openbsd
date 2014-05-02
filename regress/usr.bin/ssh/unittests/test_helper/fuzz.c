b06cced27b4864257cbbf8992215270a8a52f587
andre <andre>
2014-05-02 09:41:32 +0000

    Add missing $ on OpenBSD cvs tags, ok djm

--- regress/usr.bin/ssh/unittests/test_helper/fuzz.c
+++ regress/usr.bin/ssh/unittests/test_helper/fuzz.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD	*/
+/*	$OpenBSD: src/regress/usr.bin/ssh/unittests/test_helper/fuzz.c,v 1.3 2014/05/02 09:41:32 andre Exp $	*/
 /*
  * Copyright (c) 2011 Damien Miller <djm@mindrot.org>
  *
